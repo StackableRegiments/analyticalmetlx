54a7ee398fd768cc0375106f41ed16ee5e91e3d2
David Hagan <david.hagan@monash.edu>
2015-08-22 11:58:43 +1000

    added groupsProviders.  updated CASAuthenticator to 1.4.  Re-enabled being able to switch between CAS and SAML.    Restructured the serverConfiguration.  Removed OpenIdAuthenticator, because since google changed the rules, it probably shouldn't be permitted.  Un-hardcoded the unrestricted group, to permit a modification of default groupings, should it be desired.  Created a global and a specific user override group provider, so that we can add superusers to the system
    Former-commit-id: c827709e57b3857ed2bdff86891656500a132102

--- src/main/scala/com/metl/model/BackendAdaptor.scala
+++ src/main/scala/com/metl/model/BackendAdaptor.scala
@@ -26,6 +26,8 @@ import net.liftweb.util.Props
 import com.mongodb._
 import net.liftweb.mongodb._
 
+import scala.xml._
+
 object StackConfiguration {
   def setup = {
     val mo = new MongoOptions
@@ -44,7 +46,8 @@ object StackConfiguration {
   }
 }
 
-object MeTLXConfiguration {
+
+object MeTLXConfiguration extends PropertyReader {
   protected var configs:Map[String,Tuple2[ServerConfiguration,RoomProvider]] = Map.empty[String,Tuple2[ServerConfiguration,RoomProvider]]
   val updateGlobalFunc = (c:Conversation) => {
     getRoom("global",c.server.name) ! ServerToLocalMeTLStanza(MeTLCommand(c.server,c.author,new java.util.Date().getTime,"/UPDATE_CONVERSATION_DETAILS",List(c.jid.toString)))
@@ -52,23 +55,147 @@ object MeTLXConfiguration {
   def getRoomProvider(name:String) = {
     new HistoryCachingRoomProvider(name)
   }
-  def setupServersFromFile(filePath:String) = {
-    def setupUserWithSamlState(la: LiftAuthStateData): Unit = {
-      if ( la.authenticated ) {
-        Globals.currentUser(la.username)
-        Globals.casState.set(new CASStateData(true,la.username,(("ou","unrestricted") :: la.eligibleGroups.toList).distinct,la.informationGroups))
-      }
-    }
-    LiftAuthAuthentication.attachAuthenticator(
-      new SAMLAuthenticationSystem(
-        new SAMLAuthenticator(
-          alreadyLoggedIn = () => Globals.casState.authenticated,
-          onSuccess = setupUserWithSamlState _,
-          samlConfiguration = Globals.getSAMLconfiguration
-        )
-      )
-    )
+  def getSAMLconfiguration(propertySAML:NodeSeq) = {
+//    val propertySAML = readNode(properties, "saml")
 
+    val serverScheme = readMandatoryText(propertySAML, "serverScheme")
+    val serverName = readMandatoryText(propertySAML, "serverName")
+    val serverPort = readMandatoryText(propertySAML, "serverPort")
+
+    val samlCallbackUrl = readMandatoryText(propertySAML, "callbackUrl")
+    val idpMetadataFileName = readMandatoryText(propertySAML, "idpMetadataFileName")
+
+    val maximumAuthenticationLifetime = readMandatoryText(propertySAML, "maximumAuthenticationLifetime")
+
+    val optionOfSettingsForADFS = tryo{ maximumAuthenticationLifetime.toInt } match {
+      case Full(number:Int) => Some(SettingsForADFS(maximumAuthenticationLifetime = number.toInt))
+      case _ => None
+    }
+
+    val nodeProtectedRoutes = readNodes(readNode(propertySAML, "protectedRoutes"),"route")
+    val protectedRoutes = nodeProtectedRoutes.map(nodeProtectedRoute => {
+      nodeProtectedRoute.text :: Nil
+    }).toList
+
+    val attrTransformers = Map(readNodes(readNode(propertySAML, "informationAttributes"),"informationAttribute").flatMap(elem => elem match {
+      case e:Elem => Some((readMandatoryAttribute(e,"samlAttribute"),readMandatoryAttribute(e,"attributeType")))
+      case _ => None
+    }).toList:_*)
+    val groupMap = Map(readNodes(readNode(propertySAML, "eligibleGroups"),"eligibleGroup").flatMap(elem => elem match {
+      case e:Elem => Some((readMandatoryAttribute(e,"samlAttribute"),readMandatoryAttribute(e,"groupType")))
+      case _ => None
+    }).toList:_*)
+
+    SAMLconfiguration(
+      idpMetaDataPath = idpMetadataFileName, //getClass.getResource("/%s".format(idpMetadataFileName)).getPath,
+      serverScheme = serverScheme,
+      serverName = serverName,
+      serverPort = serverPort.toInt,
+      callBackUrl = samlCallbackUrl,
+      protectedRoutes = protectedRoutes,
+      optionOfSettingsForADFS = optionOfSettingsForADFS,
+      eligibleGroups = groupMap,
+      attributeTransformers = attrTransformers
+    )
+  }
+  protected def ifConfigured(in:NodeSeq,elementName:String,action:NodeSeq=>Unit, permitMultipleValues:Boolean = false):Unit = {
+    (in \\ elementName).theSeq match {
+      case Nil => {}
+      case element :: Nil => action(element)
+      case elements if permitMultipleValues => elements.foreach(element => action(element))
+      case _ => throw new Exception("too many %s elements in configuration file".format(elementName))
+    }
+  }
+  protected def ifConfiguredFromGroup(in:NodeSeq,elementToAction:Map[String,NodeSeq=>Unit]):Unit = {
+    var oneIsConfigured = false
+    elementToAction.keys.foreach(element => {
+      if (!oneIsConfigured){
+        ifConfigured(in,element,(n:NodeSeq) => {
+          oneIsConfigured = true
+          elementToAction(element)(n)
+        })
+      } else {
+        throw new Exception("too many elements in configuration file: %s".format(elementToAction))
+      }
+    })
+  }
+  def setupAuthorizersFromFile(filePath:String) = {
+    val propFile = XML.load(filePath)
+    val authorizationNodes = propFile \\ "properties" \\ "groupsProvider"
+    ifConfigured(authorizationNodes,"selfGroups",(n:NodeSeq) => {
+      Globals.groupsProviders = new SelfGroupsProvider() :: Globals.groupsProviders
+    },false)
+    ifConfigured(authorizationNodes,"flatFileGroups",(n:NodeSeq) => {
+      Globals.groupsProviders = GroupsProvider.createFlatFileGroups(n) :: Globals.groupsProviders
+    },true)
+  }
+  def setupAuthenticatorsFromFile(filePath:String) = {
+    val propFile = XML.load(filePath)
+    val authenticationNodes = propFile \\ "properties" \\ "authentication"
+    ifConfiguredFromGroup(authenticationNodes,Map(
+      "saml" -> {(n:NodeSeq) => {
+        def setupUserWithSamlState(la: LiftAuthStateData): Unit = {
+          if ( la.authenticated ) {
+            Globals.currentUser(la.username)
+            Globals.casState.set(new LiftAuthStateData(true,la.username,(la.eligibleGroups.toList ::: Globals.groupsProviders.flatMap(_.getGroupsFor(la.username))).distinct,la.informationGroups))
+          }
+        }
+        LiftAuthAuthentication.attachAuthenticator(
+          new SAMLAuthenticationSystem(
+            new SAMLAuthenticator(
+              alreadyLoggedIn = () => Globals.casState.authenticated,
+              onSuccess = setupUserWithSamlState _,
+              samlConfiguration = getSAMLconfiguration(n)
+            )
+          )
+        )
+      }},
+      "cas" -> {(n:NodeSeq) => {
+//        CASAuthentication.attachCASAuthenticator( 
+        LiftAuthAuthentication.attachAuthenticator(
+          new CASAuthenticationSystem(
+            new CASAuthenticator(
+              (n \\ "@realm").text,
+              (n \\ "@baseUrl").text,
+              None,
+              alreadyLoggedIn = () => Globals.casState.authenticated,
+              onSuccess = (la:LiftAuthStateData) => {
+                if ( la.authenticated ) {
+                  Globals.currentUser(la.username)
+                  Globals.casState.set(new LiftAuthStateData(true,la.username,(la.eligibleGroups.toList ::: Globals.groupsProviders.flatMap(_.getGroupsFor(la.username))).distinct,la.informationGroups))
+                }
+              }
+            ){
+              //this is where we do the appropriate setup for being behind a reverse proxy and not detecting our own location correctly.  This should be set from the xml element.
+              protected override val overrideHost:Box[String] = Empty
+              protected override val overridePort:Box[Int] = Empty
+              protected override val overrideScheme:Box[String] = Empty              
+            }
+          )
+        )
+    // )
+      }}/*,
+      "openIdAuthenticator" -> {(n:NodeSeq) => {
+        OpenIdAuthenticator.attachOpenIdAuthenticator(
+          new OpenIdAuthenticator(
+            alreadyLoggedIn = () => Globals.casState.authenticated,
+            onSuccess = (la:LiftAuthStateData) => {
+              if ( la.authenticated ) {
+                Globals.currentUser(la.username)
+                Globals.casState.set(new CASStateData(true,la.username,(la.eligibleGroups.toList ::: Globals.groupsProviders.flatMap(_.getGroupsFor(la.username))).distinct,la.informationGroups))
+              }
+            },
+            (n \\ "openIdEndpoint").map(eXml => OpenIdEndpoint((eXml \\ "@name").text,(s) => (eXml \\ "@formattedEndpoint").text.format(s),(eXml \\ "@imageSrc").text,Empty)) match {
+              case Nil => Empty
+              case specificEndpoints => Full(specificEndpoints)
+            }
+          )
+        )
+      }}
+      */
+    ))
+  }
+  def setupServersFromFile(filePath:String) = {
     MeTL2011ServerConfiguration.initialize
     LocalH2ServerConfiguration.initialize
     ServerConfiguration.loadServerConfigsFromFile(filePath,updateGlobalFunc)
@@ -90,6 +217,8 @@ object MeTLXConfiguration {
       getRoom("global",c._1.name)
       println("%s is now ready for use (%s)".format(c._1.name,c._1.isReady))
     })
+    setupAuthorizersFromFile(Globals.configurationFileLocation)
+    setupAuthenticatorsFromFile(Globals.configurationFileLocation)
     configs.values.foreach(c => LiftRules.unloadHooks.append(c._1.shutdown _))
     StackConfiguration.setup
     println(configs)
