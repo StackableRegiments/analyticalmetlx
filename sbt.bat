aa863352fb9bfe9a41f1a8e9296e68e8a4c71e70
David Hagan <david.hagan@monash.edu>
2015-10-23 11:48:13 +1100

    added a mockAuthenticator built in, so that there's no loop through another cas server etc, and updated the configuration.local.xml to give an example of how to configure it.

--- sbt.bat
+++ sbt.bat
@@ -1,1 +1,1 @@
-java -jar sbt-launch.jar 
+java -Dsbt.boot.directory="./.sbt-boot" -Dsbt.global.home="./.sbt" -Dsbt.home="./.sbt" -Dsbt.ivy.home=%IVY_HOME%\.ivy2\ -Dsbt.global.staging="./.sbt-staging" -jar sbt-launch.jar 
