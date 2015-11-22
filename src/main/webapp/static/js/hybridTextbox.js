8a7ee1311c15996af2b15ab7f0b951c70b52eca7
Chris Hagan <captain.mister.hagan@gmail.com>
2015-11-22 20:35:12 +1100

    textbox working from embed

--- src/main/webapp/static/js/hybridTextbox.js
+++ src/main/webapp/static/js/hybridTextbox.js
@@ -3,19 +3,35 @@ var MeTLText = (function(){
   var px = function(i){
     return ""+i+"px";
   };
+  var l = function(s){
+    return function(){
+      alert(s);
+    };
+  }
   return {
+    live:function(){
+      $("body").click(function(e){
+        MeTLText.append(e.pageX,e.pageY);
+      });
+    },
     append:function(x,y){
-      var id = "t_"+textBoxCount++;
-      var c = $("<div />",{
-        id:id
-      }).appendTo("body").css(
-        {
-          position:"absolute",
-          left:px(x),
-          top:px(y)
-        }
-      );
-      new SquireUI({div:"#"+id,buildPath:"/static/"});
+      var el = document.elementFromPoint(x,y);
+      var tg = el.tagName.toLowerCase();
+      if(tg == "body" || tg == "html"){
+        var id = "t_"+textBoxCount++;
+        var c = $("<div />",{
+          id:id
+        }).appendTo("body").css(
+          {
+            position:"absolute",
+            left:px(x),
+            top:px(y)
+          }
+        );
+        new SquireUI({div:"#"+id,buildPath:"/static/"});
+        return id;
+      }
+      return el.id
     }
-  };
+  }
 })();
