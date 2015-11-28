35d67045e5ac90f7df33a71f46d5146678520e4f
Chris Hagan <captain.mister.hagan@gmail.com>
2015-11-28 15:01:19 +1100

    textbox stuff

--- src/main/webapp/static/js/hybridTextbox.js
+++ src/main/webapp/static/js/hybridTextbox.js
@@ -8,35 +8,34 @@ var MeTLText = (function(){
     hideControls:function(){
       $("#topControl").hide();
       $("#bottomControl").hide();
+      return true;
     },
     showControls:function(){
       $("#topControl").show();
       $("#bottomControl").show();
+      return true;
     },
     append:function(x,y){
-      var target = document.elementFromPoint(x,y).tagName.toLowerCase();
-      if(target == "body" || target == "html"){
-        var id = "txt_"+textBoxCount++;
-        $('<textarea />',{
-          id:id,
-          name:id
-        }).appendTo('#editables');
+      var id = "txt_"+textBoxCount++;
+      $('<textarea />',{
+        id:id,
+        name:id
+      }).appendTo('#editables');
 
-        var editor = CKEDITOR.inline(id, {
-          startupFocus:true,
-          extraPlugins: 'sharedspace,youtube',
-          removePlugins: 'floatingspace,maximize,resize',
-          sharedSpaces: {
-            top: 'topControl',
-            bottom: 'bottomControl'
-          }
-        });
-        editor.on('instanceReady',function(){
-          var container = editor.container.$;
-          container.style.left = px(x);
-          container.style.top = px(y);
-        });
-      }
+      var editor = CKEDITOR.inline(id, {
+        startupFocus:true,
+        extraPlugins: 'sharedspace,youtube',
+        removePlugins: 'floatingspace,maximize,resize',
+        sharedSpaces: {
+          top: 'topControl',
+          bottom: 'bottomControl'
+        }
+      });
+      editor.on('instanceReady',function(){
+        var container = editor.container.$;
+        container.style.left = px(x);
+        container.style.top = px(y);
+      });
     }
   }
 })();
