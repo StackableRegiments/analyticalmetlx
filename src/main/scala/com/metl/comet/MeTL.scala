2ee338eb3da988349b4d017f8b4cd8533f4d69d9
David Hagan <David Hagan>
2016-06-12 22:21:26 +1000

    updated the sharing behaviour to better handle being styled by Jodi, so it's now not replacing nodes, it's only augmenting xml attrs with the appropriate behaviours

--- src/main/scala/com/metl/comet/MeTL.scala
+++ src/main/scala/com/metl/comet/MeTL.scala
@@ -387,36 +387,19 @@ class MeTLEditConversationActor extends StronglyTypedJsonActor with CometListene
                 serverConfig.updateSubjectOfConversation(conv.jid.toString,newSubject.toLowerCase)
                 Noop
               })
-              val form = new ChoiceHolder[String](choiceHolder.items.map(item => {
-                item.copy(xhtml = {
-                  val newId = nextFuncName
-                  val newXml = <label for={newId}>{item.key}</label> ++ ("input [id]" #> newId & "input *" #> NodeSeq.Empty).apply(item.xhtml \\ "input")
-                  println("newXml: %s".format(newXml))
-                  newXml
-                })
-              })).toForm
-              val labels = (form \\ "label").theSeq.toList
-              val inputs = (form \\ "input").theSeq.toList
-              val grouped = (labels ::: inputs).groupBy(el => el match {
-                case e:Elem if e.label == "input" => (e \\ "@id").text
-                case e:Elem if e.label == "label" => (e \\ "@for").text
-                case i @ <input>{_}</input> => (i \\ "@id").text
-                case i @ <input></input> => (i \\ "@id").text
-                case i @ <input/> => (i \\ "@id").text
-                case l @ <label>{_}</label> => (l \\ "@for").text
-                case l @ <label></label> => (l \\ "@for").text
-                case l @ <label/> => (l \\ "@for").text
-                case _ => ""
+              ".conversationSharingChoiceContainer" #> choiceHolder.items.sortWith((a,b) => a.key < b.key).map(item => {
+                val newId = nextFuncName
+                val inputElem = item.xhtml
+                val labelName = item.key
+                ".conversationSharingChoiceLabel [for]" #> newId & 
+                ".conversationSharingChoiceLabel *" #> Text(labelName) &
+                ".conversationSharingChoiceInputElement [id]" #> newId & 
+                ".conversationSharingChoiceInputElement [onclick]" #> (inputElem \ "@onclick").headOption.map(_.text) &
+                ".conversationSharingChoiceInputElement [type]" #> (inputElem \ "@type").headOption.map(_.text) &
+                ".conversationSharingChoiceInputElement [name]" #> (inputElem \ "@name").headOption.map(_.text) &
+                ".conversationSharingChoiceInputElement [checked]" #> (inputElem \ "@checked").headOption.map(_.text) &
+                ".conversationSharingChoiceInputElement [value]" #> (inputElem \ "@value").headOption.map(_.text)
               })
-              println("grouped the nodes: %s".format(grouped))
-              val theNodes = grouped.values.map(v => {
-                val innerXml = <span>{v.foldLeft(NodeSeq.Empty)((acc,item) => acc ++ item)}</span>
-                val choiceName = (innerXml \\ "label").text
-                (choiceName,innerXml)
-              }).toList.sortWith((a,b) => a._1 < b._1).foldLeft(NodeSeq.Empty:NodeSeq)((acc,item) => acc ++ item._2)
-              println("generated: %s".format(theNodes))
-              theNodes  
-              //labels ++ inputs
             } &
             ".conversationDelete *" #> ajaxButton("delete",() => {
               val result = serverConfig.deleteConversation(conv.jid.toString)
