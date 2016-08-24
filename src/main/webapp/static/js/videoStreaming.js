5ea1bcf63ffd75e0e1a081c507a4d07a65443d1e
David Hagan <dave@stackableregiments.com>
2016-08-24 10:53:16 +1000

    further tweaks around using an inbuilt signalling server in a shared worker

--- src/main/webapp/static/js/videoStreaming.js
+++ src/main/webapp/static/js/videoStreaming.js
@@ -1,6 +1,53 @@
+var VideoStream = function(constraints,sessionId){
+	var userId = new Date().getTime().toString();
+	var peerConnection;
+	var receiveDescriptionFunc = function(desc){
+		if (peerConnection != undefined){
+			peerConnection.setRemoteDescription(new RTCSessionDescription(desc));
+		}
+	};
+	var receiveCandidateFunc = function(candidate){
+		if (peerConnection != undefined){
+			peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+		}
+	};
+	var receive = function(message){
+		console.log("messageReceived:",message);
+		if ("candidate" in message.data){
+			receiveCandidateFunc(JSON.parse(message.candidate));
+		}
+		if ("description" in message.data){
+			receiveDescriptionFunc(JSON.parse(message.description));
+		}
+		if ("connect" in message.data){
+			startConference(message.data.isCaller);
+		}
+	};
+
+	// this is the bit which will have to be switched out for a more robust signalling mechanism
+	var signallingChannel = (function(){
+		var worker = new SharedWorker("/static/js/videoSignallingServer.js");
+		worker.onerror = function(error){
+			console.log("workerError",error);
+		};
+		worker.port.onmessage = receive;
+		console.log("starting signaller",worker);
+		var sendFunc = function(message){
+			message.sessionId = sessionId;
+			message.userId = userId;
+			console.log("sending:",message);
+			worker.port.postMessage(message);
+		};
+		sendFunc({
+			"action":"connect"
+		});
+		return {
+			send:sendFunc
+		};
+	})();
+	// that's all.
 
 
-var VideoStream = function(isCaller,constraints){
 	var localVideo = $("<video/>",{
 		width:240,
 		height:180
@@ -30,80 +77,54 @@ var VideoStream = function(isCaller,constraints){
 			}
 		}
 	};
-	var peerConnection = new RTCPeerConnection(configuration);
+	var startConference = function(isCaller){
+		if (peerConnection == undefined){
+			console.log("startingConference: ",isCaller);
+			peerConnection = new RTCPeerConnection(configuration);
 
-	peerConnection.onaddstream = function(remoteE){
-		remoteVideo.srcObject = remoteE.stream;
-		remoteVideo.play();
-	};
-	peerConnection.onicecandidate = function(e){
-		sendCandidate(e.candidate);
-	};
-	if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices){
-		navigator.mediaDevices.getUserMedia(constraints ? constraints : defaultConstraints).then(function(localStream){
-			localVideo.srcObject = localStream;
-			localVideo.play();
-			peerConnection.addStream(localStream);
-			peerConnection.onnegotiationeeded = function(negError) {
-				console.log("local negotiation requested, error:",negError); //not sure what to do about negotiation yet
+			peerConnection.onaddstream = function(remoteE){
+				remoteVideo.srcObject = remoteE.stream;
+				remoteVideo.play();
 			};
-			if (isCaller == true){
-				console.log("calling");
-				peerConnection.createOffer(offerOptions).then(function(desc){
-					console.log("creating offer:",desc);
-					peerConnection.setLocalDescription(desc);
-					sendDescription(desc);
+			peerConnection.onicecandidate = function(e){
+				sendCandidate(e.candidate);
+			};
+			if ("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices){
+				navigator.mediaDevices.getUserMedia(constraints ? constraints : defaultConstraints).then(function(localStream){
+					localVideo.srcObject = localStream;
+					localVideo.play();
+					peerConnection.addStream(localStream);
+					peerConnection.onnegotiationeeded = function(negError) {
+						console.log("local negotiation requested, error:",negError); //not sure what to do about negotiation yet
+					};
+					if (isCaller == true){
+						console.log("calling");
+						peerConnection.createOffer(offerOptions).then(function(desc){
+							console.log("creating offer:",desc);
+							peerConnection.setLocalDescription(desc);
+							sendDescription(desc);
+						});
+					} else {
+						console.log("answering");
+						peerConnection.createAnswer().then(peerConnection.remoteDescription,function(desc){
+							peerConnection.setLocalDescription(desc);
+							sendDescription(desc);
+						});
+					}
+				}).catch(function(error){
+					console.log("error getting device:",error);
 				});
-			} else {
-				console.log("answering");
-				peerConnection.createAnswer().then(peerConnection.remoteDescription,function(desc){
-					peerConnection.setLocalDescription(desc);
-					sendDescription(desc);
-				});
-			}
-		}).catch(function(error){
-			console.log("error getting device:",error);
-		});
-	}	
-	var receiveDescriptionFunc = function(desc){
-		if (peerConnection != undefined){
-			peerConnection.setRemoteDescription(new RTCSessionDescription(desc));
+			}	
 		}
 	};
-	var receiveCandidateFunc = function(candidate){
-		if (peerConnection != undefined){
-			peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-		}
-	};
-	var receive = function(message){
-		if ("candidate" in message){
-			receiveCandidateFunc(message.candidate);
-		}
-		if ("description" in message){
-			receiveDescriptionFunc(message.description);
-		}
-	};
-
-	// this is the bit which will have to be switched out for a more robust signalling mechanism
-	var signallingChannel = (function(){
-		var worker = new SharedWorker("sharedBus.js");
-		var sendFunc = function(message){
-		};
-		var receiveFunc = function(message){
-		};
-		return {
-			send:sendFunc
-	})();
-	// that's all.
-
 	var sendDescription = function(desc){
 		signallingChannel.send({
-			description:desc
+			description:JSON.stringify(desc)
 		});
 	};
 	var sendCandidate = function(candidate){
 		signallingChannel.send({
-			candidate:candidate
+			candidate:JSON.stringify(candidate)
 		});
 	};
 	var getLocalVideoFunc = function(){
@@ -136,8 +157,8 @@ var VideoTest = function(selector){
 	});
 };
 
-var StreamTest = function(videoSelector,isCaller){
-	var vs = VideoStream(isCaller);
+var StreamTest = function(videoSelector,sessionId){
+	var vs = VideoStream(undefined,sessionId);
 	var localVideo = vs.getLocalVideo();
 	var remoteVideo = vs.getRemoteVideo();
 	$(videoSelector).append(localVideo).append(remoteVideo);
