62d541755982d8b3d7b79dff80a7dc348dca3f2f
David Hagan <dave@stackableregiments.com>
2016-09-13 19:02:25 +1000

    added the google closure compiler's jar file to the project as a way of minifying the files.  Including it in the git repo so that it's available to all the devs and the project is ready to build and run immediately.

--- src/main/webapp/static/js/minify.sh
+++ src/main/webapp/static/js/minify.sh
@@ -1,8 +1,10 @@
+echo "execute this from the src/main/webapp/static/js directory"
 for file in $(ls)
 do
     case $file in
         *~) ;;
         *.min.js) ;;
-        *.js) java -jar /c/lang/closureCompiler/closure-compiler-v20160713.jar $file --js_output_file=min/$file ;;
+##        *.js) java -jar /c/lang/closureCompiler/closure-compiler-v20160713.jar $file --js_output_file=min/$file ;;
+        *.js) java -jar ../../../../../tools/closure-compiler-v20160713.jar $file --js_output_file=min/$file ;;
     esac
 done
