9b32528970c3ae2667e5f1319cb44cb33ba87458
Chris Hagan <chris@stackableregiments.com>
2016-10-07 22:28:49 +1100

    That didn't work

--- wdio.conf.js
+++ wdio.conf.js
@@ -127,7 +127,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/testrunner/reporters.html
-    reporters: ['dot','mocha-sauce-notifying'],
+    reporters: ['dot'],
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
