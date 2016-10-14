2d4cd7155a26232e894b9634387ddf70a53fc237
Chris Hagan <chris@stackableregiments.com>
2016-10-14 22:43:56 +1100

    repairing suace

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,6 +21,8 @@ var config = {
 if(process.env.CI){
     config.services.push('sauce');
     config.sauceConnect = true;
+    config.user = process.env.SAUCE_USERNAME;
+    config.key = process.env.SAUCE_ACCESS_KEY;
 }
 
 exports.config = config;
