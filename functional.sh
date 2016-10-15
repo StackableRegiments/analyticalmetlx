e084aa8b6f9329ab5349298a4dff9494a0821e26
Chris Hagan <chris@stackableregiments.com>
2016-10-15 21:57:19 +1100

    It looks like Sauce might not be able to do the fine grained input we need.

--- functional.sh
+++ functional.sh
@@ -9,10 +9,17 @@ npm install wdio-spec-reporter
 npm install lodash
 npm install wdio
 
-wmic process where "name like '%java%'" delete
-java -jar -Dwebdriver.chrome.driver=./tools/chromedriver/2.24-x64-chromedriver ./tools/selenium-2.53.1-server.jar &
+if $SNAP_CI
+then
+    java -jar -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver ./tools/selenium-2.53.1-server.jar &
+else
+    wmic process where "name like '%java%'" delete
+    java -jar -Dwebdriver.chrome.driver=./tools/chromedriver/2.24-x64-chromedriver ./tools/selenium-2.53.1-server.jar &
+fi
 ./sbt.sh container:launch &
 { tail -n +1 -f debug.log & } | sed -n '/bootstrap.liftweb.Boot - started/q'
 echo "Server launched"
 ./node_modules/.bin/wdio wdio.${MODE}.conf.js
-wmic process where "name like '%java%'" delete
+if ! $SNAP_CI ; then
+    wmic process where "name like '%java%'" delete
+fi
--- wdio.conf.js
+++ wdio.conf.js
@@ -18,12 +18,13 @@ var config = {
     }
 }
 
+/*
 if(process.env.CI){
     config.services.push('sauce');
     config.sauceConnect = true;
     config.user = process.env.SAUCE_USERNAME;
     config.key = process.env.SAUCE_ACCESS_KEY;
 }
-
+*/
 exports.config = config;
 
