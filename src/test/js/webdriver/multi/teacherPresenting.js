532a973af2b24e9e28418addb19fac597d085b30
Doug Hagan <doug@stackableregiments.com>
2016-10-17 12:04:34 +1100

    METL-156: Renamed tests to specify actor.

--- src/test/js/webdriver/multi/teacherPresenting.js
+++ src/test/js/webdriver/multi/teacherPresenting.js
@@ -25,7 +25,7 @@ var doubleClick = function(user,x,y){
 describe('When a teacher presents, ', function() {
     var teacherT = board(teacher);
     var studentT = board(student);
-    it('they should find the application', function () {
+    it('the teacher and student should find the application', function () {
         browser.url('/board');
     });
 
@@ -80,25 +80,25 @@ describe('When a teacher presents, ', function() {
         teacher.click(".newConversationTag");
         teacher.waitForExist("#board");
     });
-    it("the student should able to find the conversation",function(){
+    it("the student should find the conversation",function(){
         student.setValue("#conversationSearchBox > input","teacher");
         student.click("#searchButton");
         student.pause(1000);
         student.click(".newConversationTag");
         student.waitForExist("#board");
     });
-    it("should all be able to verify that the board is blank", function(){
+    it("both should see a blank board", function(){
         assert.equal(_.keys(teacherT.texts).length,0);
         assert.equal(_.keys(studentT.texts).length,0);
     });
-    it("should insert a paragraph",function(){
+    it("the teacher should insert a paragraph",function(){
         teacherT.textMode.click();
         teacherT.keyboard(50,50,"This is a paragraph of text which is being typed programatically.  It runs over multiple lines.");
         assert.equal(_.keys(teacherT.texts).length,1);
         assert.equal(teacherT.textStanzas[_.keys(teacherT.texts)[0]].words.length,[
             "Consistently sized run"].length);
     });
-    it("should highlight a word and enlarge it",function(){
+    it("the teacher should highlight a word and enlarge it",function(){
         doubleClick(teacher,100,100);
         teacher.waitUntil(function(){/*Paragraph*/
             var r = teacherT.selectedRanges[0];
@@ -137,14 +137,14 @@ describe('When a teacher presents, ', function() {
                 ["Before","Enlarged","After","Red","After","Enlarged","After"].length;
         });
     });
-    it("should create another textbox",function(){
+    it("the teacher should create another textbox",function(){
         teacherT.keyboard(600,500,"This is a second paragraph.  It exists to be differentiated from the first paragraph.");
         assert.equal(_.keys(teacherT.texts).length,2);
         assert.equal(_.keys(teacherT.textStanzas).length,2);
         assert.equal(teacherT.textStanzas[_.keys(teacherT.texts)[1]].words.length,[
             "Consistently sized run"].length);
     });
-    it("should be able to drag their new textbox",function(){
+    it("the teacher should drag their new textbox",function(){
         var active = teacherT.textStanzas[_.keys(teacherT.texts)[1]];
         assert.equal(active.x,600);
         assert.equal(active.y,500);
@@ -155,7 +155,7 @@ describe('When a teacher presents, ', function() {
         assert.equal(active.x,100);
         assert.equal(active.y,250);
     });
-    it("be able to rescale all the font in their new textbox",function(){
+    it("the teacher should rescale all the font in their new textbox",function(){
         var active = teacherT.textStanzas[_.keys(teacherT.texts)[1]];
         assert.equal(active.width,240);
         assert.equal(active.x,100);
@@ -168,15 +168,15 @@ describe('When a teacher presents, ', function() {
         assert(within(active.width,437,2));
         assert.equal(active.words[0].size, 55);
     });
-    it("should scroll up on swipe out",function(){
+    it("the teacher should scroll up on swipe out",function(){
         teacherT.swipeUp();
     });
-    if("should be able to reselect their box",function(){
+    if("the teacher should reselect their box",function(){
         teacher.moveToObject("#board",200,300);
         teacher.leftClick();
         assert.equal(teacherT.interactables.resizeFree.length,1);
     });
-    it("should be able to resize their box rewrapping instead of rescaling the text",function(){
+    it("the teacher should resize their box rewrapping instead of rescaling the text",function(){
         var handle = teacherT.interactables.resizeFree[0];
         teacher.pause(ANIMATION_DELAY);
         teacherT.drag(handle,{x:200,y:0});
@@ -186,7 +186,7 @@ describe('When a teacher presents, ', function() {
         assert.equal(active.words[0].size, 55);
         assert(within(active.width,638,2));
     });
-    it("should be able to draw ink", function(){
+    it("the teacher should be able to draw ink", function(){
         teacherT.inkMode.click();
 
         var inkStanzasBefore = _.filter(teacherT.inkStanzas,function(inkStanza){return inkStanza.author == "teacher";}).length;
@@ -210,7 +210,7 @@ describe('When a teacher presents, ', function() {
         }
         assert.equal(_.keys(teacherT.inkStanzas).length,4);
     });
-    it("should be able to add an image",function(){
+    it("the teacher should add an image",function(){
         assert.equal(_.keys(teacherT.imageStanzas).length,0);
         teacherT.imageMode.click();
         teacher.click("#board");
@@ -219,10 +219,10 @@ describe('When a teacher presents, ', function() {
             return _.keys(teacherT.imageStanzas).length == 1;
         },5000);
     });
-    it("should have their new image selected when it appears",function(){
+    it("the teacher should have their new image selected when it appears",function(){
         assert.equal(_.keys(teacherT.selection.images).length,1);
     });
-    it("should be able to clear their selection by clicking on an empty spot on the canvas",function(){
+    it("the teacher should clear their selection by clicking on an empty spot on the canvas",function(){
         teacherT.selectMode.click();
         teacher.leftClick("#board",1,1);
         assert.equal(_.keys(teacherT.selection.inks).length,0);
@@ -231,7 +231,7 @@ describe('When a teacher presents, ', function() {
         assert.equal(_.keys(teacherT.selection.videos).length,0);
         assert.equal(_.keys(teacherT.selection.images).length,0);
     });
-    it("should select all the items that are under their mouse when they click the board",function(){
+    it("the teacher should select all the items that are under their mouse when they click the board",function(){
         teacher.leftClick("#board",250,250);
         teacher.waitUntil(function(){
             var sel = teacherT.selection;
@@ -242,27 +242,27 @@ describe('When a teacher presents, ', function() {
                 _.keys(sel.images).length == 1;
         });
     });
-    it("should have their students see all their public elements",function(){
+    it("the student should see all public teacher-created elements",function(){
         student.waitUntil(function(){
             return _.keys(studentT.textStanzas).length == 2 &&
                 _.keys(studentT.inkStanzas).length == 4 &&
                 _.keys(studentT.imageStanzas).length == 1;
         });
     });
-    it("should be able to delete their selected elements",function(){
+    it("the teacher should delete their selected elements",function(){
         teacherT.deleteSelection.click();
     });
-    it("should have their students see all their remaining public elements",function(){
+    it("the student should see all remaining public teacher-created elements",function(){
         student.waitUntil(function(){
             return _.keys(studentT.imageStanzas).length == 0;
         });
         assert.equal(_.keys(studentT.textStanzas).length,1);
         assert.equal(_.keys(studentT.inkStanzas).length,3);
     });
-    it("should have their participants view show the themes so far introduced in their conversation",function(){
+    it("the teacher should have their participants view show the themes so far introduced in their conversation",function(){
         assert(teacherT.cloudData.length > 0);
     });
-    it("should not have their students see their private content",function(){
+    it("the teacher should create a private image",function(){
         teacherT.privateMode.click();
         teacherT.imageMode.click();
         teacher.click("#board");
@@ -272,7 +272,10 @@ describe('When a teacher presents, ', function() {
         });
         assert.equal(_.keys(studentT.imageStanzas).length,0);
     });
-    it("should be able to restore their deleted content from the recycle bin",function(){
+    it("the student should not see private teacher-created content",function(){
+        assert.equal(_.keys(studentT.imageStanzas).length,0);
+    });
+    it("the teacher should restore deleted content from the recycle bin",function(){
         teacherT.menuButton.click();
         teacherT.recycleBinMenu.click();
         teacher.waitUntil(function(){
