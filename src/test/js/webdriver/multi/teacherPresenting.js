cd0ae4b574fb53a07743e5ab5280bb94ea3b503f
Doug Hagan <doug@stackableregiments.com>
2016-10-17 11:36:33 +1100

    METL-156: Refactoring with pageobjects.

--- src/test/js/webdriver/multi/teacherPresenting.js
+++ src/test/js/webdriver/multi/teacherPresenting.js
@@ -22,21 +22,49 @@ var doubleClick = function(user,x,y){
     user.leftClick();
     user.leftClick();
 }
-describe('When a teacher presents, they', function() {
+describe('When a teacher presents, ', function() {
     var teacherT = board(teacher);
     var studentT = board(student);
-    it('should find the application', function () {
+    it('they should find the application', function () {
         browser.url('/board');
     });
-    it('should successfully login', function () {
-        teacher.setValue('input[type=text]', 'teacher');
-        student.setValue('input[type=text]', 'student');
+
+    var teacherLoginPage = LoginPage(teacher);
+    var studentLoginPage = LoginPage(student);
+    var teacherName = 'test.teacher.' + Math.floor(Math.random() * 10000);
+
+    var teacherConversationsPage = ConversationsPage(teacher);
+    var studentConversationsPage = ConversationsPage(student);
+
+    it('the teacher should successfully login', function () {
+        teacherLoginPage.username.setValue(teacherName);
+        teacherLoginPage.submit();
+        assert(teacherConversationsPage.waitForSearchBox());
     });
-    it('should be able to submit the login form', function () {
-        browser.click('input[type=submit]');
-        browser.waitForExist("#conversationSearchBox");
+    it('the student should successfully login', function () {
+        studentLoginPage.username.setValue('test.student');
+        studentLoginPage.submit();
+        assert(studentConversationsPage.waitForSearchBox());
     });
-    it("should be able to create and join a conversation", function() {
+/*
+    it("should be able to import and join a conversation", function() {
+        teacher.waitForExist("#importConversationButton");
+        var previousConversations = teacher.execute("return Conversations.getConversationListing()").value;
+        teacher.click("#createConversationButton");
+        teacher.waitUntil(function(){
+            return teacher.execute("return Conversations.getConversationListing()").value.length == (previousConversations.length + 1);
+        },5000,"expected a new conversation to appear");
+        var newConversations = _.filter(teacher.execute("return Conversations.getConversationListing()").value,function(nc){
+            return !_.some(previousConversations,function(pc){
+                return pc == nc;
+            });
+        });
+        assert.ok(newConversations.length > 0,"expected there to be at least 1 new conversation");
+        teacher.click(".newConversationTag");
+        teacher.waitForExist("#board");
+    });
+*/
+    it("the teacher should be able to create and join a conversation", function() {
         teacher.waitForExist("#createConversationButton");
         var previousConversations = teacher.execute("return Conversations.getConversationListing()").value;
         teacher.click("#createConversationButton");
@@ -52,7 +80,7 @@ describe('When a teacher presents, they', function() {
         teacher.click(".newConversationTag");
         teacher.waitForExist("#board");
     });
-    it("should have their students able to find the conversation",function(){
+    it("the student should able to find the conversation",function(){
         student.setValue("#conversationSearchBox > input","teacher");
         student.click("#searchButton");
         student.pause(1000);
