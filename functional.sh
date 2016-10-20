e3e4703518d0fb956a40c134adf8f9856c98ad5e
Doug Hagan <doug@stackableregiments.com>
2016-10-20 16:14:05 +1100

    Modified functional.sh for use in Snap (incorporated current Snap debug/config).

--- functional.sh
+++ functional.sh
@@ -9,19 +9,35 @@ touch debug.log
 if [[ "$SNAP_CI" ]]; then
     echo "Running in CI"
 
-    npm install sprintf-js
-    npm install wdio-mocha-framework
-    npm install wdio-spec-reporter
-    npm install lodash
-    npm install wdio
-
     sbt -Xms1536m -Xmx1536m -Dlogback.configurationFile=/var/snap-ci/repo/config/logback.xml -Dmetlx.configurationFile=/var/snap-ci/repo/config/configuration.ci.xml container:launch &
     sleep 150
     #{ tail -n +1 -f debug.log & } | sed -n '/bootstrap.liftweb.Boot - started/q'
 
+	echo $! > running.pid
+	sleep 30
+	ps -ef | grep java
+	echo "serverStarted: $(cat running.pid)"
+	curl -vvv http://localhost:8080/serverStatus
+	
+    npm install wdio
+    npm install lodash
+    npm install sprintf-js
+    npm install wdio-mocha-framework
+    npm install wdio-spec-reporter
+    npm install wdio-selenium-standalone-service
+	#npm install wdio-sauce-service
+	#npm install sauce-connect
+	#npm install mocha-sauce-notifying-reporter
+
     echo "Starting WDIO"
     java -jar -Dwebdriver.chome.driver=/usr/local/bin/chromedriver ./tools/selenium-2.53.1-server.jar &
     wdio wdio.${MODE}.conf.js
+	
+	echo "stopping server: $(cat running.pid)"
+	pkill -KILL -P $(cat running.pid)
+	kill -KILL $(cat running.pid)
+	rm running.pid
+	ps -ef | grep java
 else
     echo "Running on local"
     wmic process where "name like '%java%'" delete
