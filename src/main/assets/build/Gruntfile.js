5a759a10e9c793279b2e6996bbe8bc3627fc9d74
Chris Hagan <chris@stackableregiments.com>
2016-10-23 15:30:37 +1100

    METL-193 METL-188 Gruntfile now needs to be parameterized with a theme.  Application now needs to choose a theme.  Fixed black on black bug

--- src/main/assets/build/Gruntfile.js
+++ src/main/assets/build/Gruntfile.js
@@ -1,63 +1,65 @@
 module.exports = function( grunt ) {
     // Load grunt plugins
+    grunt.config('theme', grunt.option('theme') || process.env.GRUNT_ENV || 'neutral');
+
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
 
-    grunt.initConfig({
+    var config = {
         lint: {
             all: ['../js/*.js', '../js/vendor/*.js']
         },
         copy: {
             main: {
-              files: [
-                // includes files within path and its sub-directories
-                {expand:true, flatten:true, cwd: '../styles/images',  src: ['**'], dest: '../../webapp/static/assets/styles/images/'},
-                {src: ['../js/vendor/jquery-1.11.3.min.js'], dest: '../../webapp/static/assets/js/vendor/jquery.min.js'}
-              ]
+                files: [
+                    // includes files within path and its sub-directories
+                    {expand:true, flatten:true, cwd: '../styles/images',  src: ['**'], dest: '../../webapp/static/assets/styles/images/'},
+                    {src: ['../js/vendor/jquery-1.11.3.min.js'], dest: '../../webapp/static/assets/js/vendor/jquery.min.js'}
+                ]
             }
-          },
+        },
         concat: {
             options: {
-              separator: ';',
+                separator: ';',
             },
             dist: {
-              src: [
+                src: [
                     '../js/vendor/modernizr.2.8.3.min.js',
                     '../js/mod-popout.js'
-                    ],
-              dest: 'source/js/metl-front-end-core.js'
+                ],
+                dest: 'source/js/metl-front-end-core.js'
             },
         },
-        
+
         uglify: {
             options: {
                 mangle: false
             },
             my_target: {
                 files: {
-                  '../../webapp/static/assets/js/metl-front-end-core.min.js': ['source/js/metl-front-end-core.js']
+                    '../../webapp/static/assets/js/metl-front-end-core.min.js': ['source/js/metl-front-end-core.js']
                 }
-              }
-            },
+            }
+        },
 
         less: {
             development: {
-            options: {
-              compress: true,
-              yuicompress: true,
-              optimization: 2
-            },
-            files: {
-              // target.css file: source.less file
-              "../../webapp/static/assets/styles/main.css": "../main.less"
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    optimization: 2,
+                    modifyVars:{
+                        theme: grunt.config('theme')
+                    }
+                },
+                files: {}
             }
-          }
         }
-        
-        
-    });
+    }
+    config.less.development.files["../../webapp/static/assets/styles/"+grunt.config('theme')+"/main.css"] = "../main.less";
+    grunt.initConfig(config);
 
     // Default build
     grunt.registerTask('default', [ 'copy', 'concat', 'uglify', 'less']);
