fb2f23e75260ef0aff58f4bd848f344506608bda
Doug Hagan <doug@stackableregiments.com>
2016-11-23 12:51:57 +1100

    Revert "METL-230: found that google-image drag'n'drop result in eventhandlers surviving and being fired.  Fixed to prevent that."
    This reverts commit 7a82ff2712465f31cef7b86f594ef7f53bd6c540.

--- src/main/webapp/static/js/onLoad.js
+++ src/main/webapp/static/js/onLoad.js
@@ -845,12 +845,6 @@ $(function(){
 						value:df.getData(type)
 					};
 				});
-				var extractHtmlNodes = function(raw){
-					var safeRoot = $("<span/>");
-					safeRoot[0].innerHTML = raw;
-					return safeRoot;
-					//return $($.parseHTML(raw,null,false));
-				};
 				var conditionallyActOn = function(coll,itemPred,action){
 					var elem = _.find(coll,itemPred);
 					if (elem != undefined && elem != null){
@@ -879,10 +873,9 @@ $(function(){
 								var html = _.find(dataSets,function(ds){
 									return ds.key == type;
 								}).value;
-								//console.log("beforeJoin",html);
-								//var htmlElem = $(html);
-								var htmlElem = extractHtmlNodes(html);
+								var htmlElem = $(html);
 								var yOffset = 0;
+								html = _.join(_.map(htmlElem,function(he){return he.outerHTML;}),"");
 								_.forEach(htmlElem.find("img"),function(imgNode){
 									try {
 										Modes.image.handleDroppedSrc(imgNode.src,x,y + yOffset);	
@@ -891,12 +884,9 @@ $(function(){
 										errorAlert("Error dropping image","The source server you're draggin the image from does not want to allow dragging the image directly across into MeTL.  You may need to download the image first and then upload it.  " + e);
 									}
 								});
-								/*
 								if (htmlElem.text().trim().length > 1){
-									var safeHtml = _.join(_.map(htmlElem,function(he){return he.outerHTML;}),"");
-									Modes.text.handleDrop(safeHtml,x,y + yOffset);
+									Modes.text.handleDrop(html,x,y + yOffset);
 								}
-								*/
 							}
 						},
 						{
@@ -971,8 +961,9 @@ $(function(){
 					*/
 					conditionallyActOn(availableTypes,function(label){return label == "text/html";},function(type,html){
 						if (!handled){
-							var htmlElem = extractHtmlNodes(html);
+							var htmlElem = $(html);
 							var yOffset = 0;
+							html = _.join(_.map(htmlElem,function(he){return he.outerHTML;}),"");
 							_.forEach(htmlElem.find("img"),function(imgNode){
 								try {
 									Modes.image.handleDroppedSrc(imgNode.src,x,y + yOffset);	
@@ -982,8 +973,7 @@ $(function(){
 								}
 							});
 							if (htmlElem.text().trim().length > 1){
-								var safeHtml = _.join(_.map(htmlElem,function(he){return he.outerHTML;}),"");
-								Modes.text.handleDrop(safeHtml,x,y + yOffset);
+								Modes.text.handleDrop(html,x,y + yOffset);
 							}
 							handled = true;
 						}
