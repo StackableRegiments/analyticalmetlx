d33acf87ad6620e9cc56b7f0f5582944b294307a
Chris Hagan <chris@stackableregiments.com>
2016-11-28 23:07:32 -0500

    252: Filters working on groups plugin

--- src/main/webapp/static/js/plugins.js
+++ src/main/webapp/static/js/plugins.js
@@ -37,7 +37,7 @@ var Plugins = (function(){
             }
         })(),
         "Groups":(function(){
-            var container = $("<div />").css({display:"flex"});
+            var overContainer = $("<div />");
             var button = function(icon,content,behaviour){
                 var b = $("<button />",{
                     class:sprintf("%s btn-icon fa",icon),
@@ -51,19 +51,41 @@ var Plugins = (function(){
             };
             return {
                 style:".groupsPluginMember{margin-left:0.5em;}"+
-                    " .groupsPluginGroupContainer{display:table-cell;display:flex;}"+
-                    " .groupsPluginGroupContainer .icon-txt{color:white;}"+
+                    " .groupsPluginGroupContainer{display:flex;margin-right:1em;}"+
+                    " .groupsPluginGroupContainer .icon-txt, .groupsPluginAllGroupsControls .icon-txt, .groupsPluginAllGroupsControls .fa{color:white;}"+
                     " .groupsPluginGroup{display:inline-block;text-align:center;vertical-align:top;}"+
                     " .groupsPluginControls button, .groupsPluginControls .icon-txt{padding:0;color:white;margin-top:0;}"+
-                    " .groupsPluginControls button{display:block;}",
+                    " .isolateGroup label{margin-top:1px;}"+
+                    " .groupsPluginControls button{display:block;}"+
+                    " .groupsPluginAllGroupsControls{margin-bottom:0.5em;border-bottom:0.5px solid white;padding-left:1em;display:flex;}",
                 load:function(bus,params) {
                     var render = function(){
                         try{
-                            container.empty();
+                            overContainer.empty();
                             var slide = Conversations.getCurrentSlide();
                             if(slide){
                                 var groupSet = slide.groupSet;
                                 if(groupSet){
+                                    var allControls = $("<div />",{
+                                        class:"groupsPluginAllGroupsControls"
+                                    }).append($("<input />",{
+                                        type:"radio",
+                                        name:"groupView",
+                                        id:"showAll"
+                                    }).click(function(){
+                                        _.each(groupSet.groups,function(g){
+                                            ContentFilter.setFilter(g.id,true);
+                                        });
+                                    })).append($("<label />",{
+                                        for:"showAll"
+                                            }).css({
+                                                "flex-grow":0
+                                            }).append($("<span />",{
+                                                class:"icon-txt",
+                                                text:"Show all"
+                                            }))).appendTo(overContainer);
+                                    button("fa-bar-chart","Apply scores",function(){}).css({"margin-top":0}).appendTo(allControls);
+                                    var container = $("<div />").css({display:"flex"}).appendTo(overContainer);
                                     _.each(groupSet.groups,function(group){
                                         var gc = $("<div />",{
                                             class:"groupsPluginGroupContainer"
@@ -72,7 +94,7 @@ var Plugins = (function(){
                                         var controls = $("<div />",{
                                             class:"groupsPluginControls groupsPluginGroup"
                                         }).appendTo(gc);
-                                        button("fa-share-square isolate","Submit screen",function(){}).appendTo(controls);
+                                        button("fa-share-square","Submit screen",function(){}).appendTo(controls);
                                         $("<input />",{
                                             type:"range",
                                             orient:"vertical",
@@ -91,8 +113,14 @@ var Plugins = (function(){
                                         var isolate = $("<div />",{
                                             class:"isolateGroup"
                                         }).append($("<input />",{
-                                            type:"checkbox",
+                                            type:"radio",
+                                            name:"groupView",
                                             id:id
+                                        }).change(function(){
+                                            _.each(groupSet.groups,function(g){
+                                                ContentFilter.setFilter(g.id,false);
+                                            });
+					    ContentFilter.setFilter(group.id,true);
                                         })).append($("<label />",{
                                             for:id
                                         }).append($("<span />",{
@@ -118,7 +146,7 @@ var Plugins = (function(){
                     };
                     bus.afterJoiningSlide["Groups plugin"] = render;
                     bus.conversationDetailsReceived["Groups plugin"] = render;
-                    return container;
+                    return overContainer;
                 },
                 initialize:function(){}
             };
