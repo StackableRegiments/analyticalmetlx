2542c9e0a3594386043db9f3df4e028ec87ee155
David Hagan <dave@stackableregiments.com>
2016-12-29 00:28:02 +1100

    METL-305: now there's a valid briding mechanic from the room, I hope.

--- src/main/scala/com/metl/model/TokBoxManager.scala
+++ src/main/scala/com/metl/model/TokBoxManager.scala
@@ -3,6 +3,25 @@ package com.metl.model
 import com.opentok.android.{OpentokError,Session,Stream,Connection}
 import net.liftweb.common._
 
+object TokBoxHelper {
+  def getSessionNamesFor(room:RoomMetaData):List[String] = {
+    room match {
+      case cr:ConversationRoom => List(cr.jid)
+      case gs:SlideRoom if !gs.s.groupSet.isEmpty => gs.s.groupSet.flatMap(_.groups.map(_.id))
+      case _ => Nil
+    }
+  }
+  def getMonitors(room:RoomMetaData):List[TokBoxMonitor] = {
+    for {
+      tb <- Globals.tokBox.toList
+      sn <- getSessionNamesFor(room)
+      s <- tb.getSessionToken(sn,TokRole.Moderator).right.toOption.toList
+    } yield {
+      new TokBoxMonitor(s.apiKey.toString,s.sessionId,s.token)
+    }
+  }
+}
+
 class TokBoxMonitor(apiKey:String,sessionId:String,token:String) extends Session.SessionListener with Session.ArchiveListener with Session.ConnectionListener with Session.ReconnectionListener with Logger {
     protected val mSession:Session = {
       val session = new Session(new android.test.mock.MockContext(), apiKey,sessionId)
