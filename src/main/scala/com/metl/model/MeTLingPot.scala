863280b8cf1216070018b9aff0f8d290ffe75bfa
David Hagan <dave@stackableregiments.com>
2017-01-13 14:42:48 +1100

    METL-507: forgot an override

--- src/main/scala/com/metl/model/MeTLingPot.scala
+++ src/main/scala/com/metl/model/MeTLingPot.scala
@@ -303,7 +303,7 @@ object MeTLingPot {
 class SmartGroupsProvider(override val storeId:String, endpoint:String,region:String,iamAccessKey:String,iamSecretAccessKey:String,apiGatewayApiKey:Option[String],groupSize:Int) extends GroupsProvider(storeId) {
   import com.metl.liftAuthenticator._
   override val canQuery:Boolean = true
-  val canRestrictConversations:Boolean = false
+  override val canRestrictConversations:Boolean = false
   protected val clientFactory = new APIGatewayClient(endpoint,region,iamAccessKey,iamSecretAccessKey,apiGatewayApiKey)
   def client:MetlingPotInputItem = clientFactory.client
 
