06e1a8b2314dd1eb2635e818a1dbfa0e38eb1059
Doug Hagan <doug@stackableregiments.com>
2017-01-19 15:48:37 +1100

    SITE-22: Privatised everything via let-over-lambda.

--- docs/javascripts/sidebar.js
+++ docs/javascripts/sidebar.js
@@ -1,52 +1,54 @@
-function clearSidebar() {
-    Cookies.set('sidebar', []);
-}
-
-function getSidebar() {
-    if (!Array.isArray(Cookies.getJSON('sidebar'))) {
-        clearSidebar();
+(function() {
+    function clearSidebar() {
+        Cookies.set('sidebar', []);
     }
-    return Cookies.getJSON('sidebar');
-}
 
-function isExpanded(checkbox) {
-    var id = $(checkbox).attr('id');
-
-    var sidebar = getSidebar();
-    for (var i = 0; i < sidebar.length; i++) {
-        if (sidebar[i].name == id && sidebar[i].value == true)
-            return 'checked';
-    }
-    return '';
-}
-
-function setExpanded(checkbox) {
-    var id = $(checkbox).attr('id');
-    var checked = $(checkbox).is(':checked');
-    // alert('id = ' + id + ", checked = " + checked );
-
-    var found = false;
-    var checkedArray = getSidebar();
-    for (var i = 0; i < checkedArray.length; i++) {
-        if (checkedArray[i].name == id) {
-            checkedArray[i].value = checked;
-            found = true;
-            break;
+    function getSidebar() {
+        if (!Array.isArray(Cookies.getJSON('sidebar'))) {
+            clearSidebar();
         }
-    }
-    if (!found) {
-        checkedArray.push({name: id, value: checked});
+        return Cookies.getJSON('sidebar');
     }
 
-    Cookies.set('sidebar', checkedArray);
-}
+    function isExpanded(checkbox) {
+        var id = $(checkbox).attr('id');
 
-function checkAllBoxes(selector) {
-    $(selector).each(function (index, elem) {
-        var checkbox = $(elem);
-        checkbox.prop("checked", isExpanded(checkbox));
-        checkbox.on("click", function(){setExpanded(checkbox);});
-    });
-}
+        var sidebar = getSidebar();
+        for (var i = 0; i < sidebar.length; i++) {
+            if (sidebar[i].name == id && sidebar[i].value == true)
+                return 'checked';
+        }
+        return '';
+    }
 
-// alert(Cookies.get('sidebar'));
+    function setExpanded(checkbox) {
+        var id = $(checkbox).attr('id');
+        var checked = $(checkbox).is(':checked');
+
+        var found = false;
+        var checkedArray = getSidebar();
+        for (var i = 0; i < checkedArray.length; i++) {
+            if (checkedArray[i].name == id) {
+                checkedArray[i].value = checked;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            checkedArray.push({name: id, value: checked});
+        }
+
+        Cookies.set('sidebar', checkedArray);
+    }
+
+    function checkAllBoxes(selector) {
+        $(selector).each(function (index, elem) {
+            var checkbox = $(elem);
+            checkbox.prop("checked", isExpanded(checkbox)).on("click", function () {
+                setExpanded(checkbox);
+            });
+        });
+    }
+
+    $(function(){checkAllBoxes("#sidebar input[type=checkbox]")});
+})();
