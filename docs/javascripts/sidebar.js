145650dead48689ba717070d23353387a69e474c
Doug Hagan <doug@stackableregiments.com>
2017-01-19 14:58:22 +1100

    SITE-22: Sidebar now remembers expand/collapse state in a session-scoped cookie.

---
+++ docs/javascripts/sidebar.js
@@ -0,0 +1,44 @@
+function clearSidebar() {
+    Cookies.set('sidebar', []);
+}
+
+function getSidebar() {
+    if (!Array.isArray(Cookies.getJSON('sidebar'))) {
+        clearSidebar();
+    }
+    return Cookies.getJSON('sidebar');
+}
+
+function isExpanded(checkbox) {
+    var id = $(checkbox).attr('id');
+
+    var sidebar = getSidebar();
+    for (var i = 0; i < sidebar.length; i++) {
+        if (sidebar[i].name == id && sidebar[i].value == true)
+            return 'checked';
+    }
+    return '';
+}
+
+function setExpanded(checkbox) {
+    var id = $(checkbox).attr('id');
+    var checked = $(checkbox).is(':checked');
+    // alert('id = ' + id + ", checked = " + checked );
+
+    var found = false;
+    var checkedArray = getSidebar();
+    for (var i = 0; i < checkedArray.length; i++) {
+        if (checkedArray[i].name == id) {
+            checkedArray[i].value = checked;
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        checkedArray.push({name: id, value: checked});
+    }
+
+    Cookies.set('sidebar', checkedArray);
+}
+
+// alert(Cookies.get('sidebar'));
