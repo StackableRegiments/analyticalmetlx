cf03b7ebd75eed26ca9bb56879abdf495f785ebe
Doug Hagan <doug@stackableregiments.com>
2017-02-10 12:10:38 +1100

    METL-276: METL-276: Further refactoring.

--- src/main/scala/com/metl/snippet/GoogleAnalytics.scala
+++ src/main/scala/com/metl/snippet/GoogleAnalytics.scala
@@ -8,26 +8,10 @@ class GoogleAnalytics {
   val gaFunction = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');"
 
   def render: NodeSeq = {
-    val hasStackable = Globals.googleAnalytics.nonEmpty
-    val hasClient = Globals.clientGoogleAnalytics.nonEmpty
-    if( hasStackable || hasClient ) {
+    <script>{gaFunction}</script> ++ List(Globals.googleAnalytics, Globals.clientGoogleAnalytics).flatMap(key =>
       <script>
-        {gaFunction}
-      </script>
-    }
-
-    if (hasStackable) {
-      <script>
-        ga('create', ' {Globals.googleAnalytics} ', 'auto');
-        ga('send', 'pageview');
-      </script>
-    }
-
-    if (hasClient) {
-      <script>
-        ga('create', ' {Globals.clientGoogleAnalytics} ', 'auto');
-        ga('send', 'pageview');
-      </script>
-    }
+        ga('create', '{key._2}', 'auto', '{key._1}');
+        ga('{key._1}.send', 'pageview');
+      </script>).foldLeft(NodeSeq.Empty)(_ ++ _)
   }
 }
