85db5cb2f050a87a6ac5c2214c26d4be9b384a63
Doug Hagan <doug@stackableregiments.com>
2017-03-09 12:46:28 +1100

    METL-569: Finalised SQL.

--- src/main/scala/com/metl/view/ReportHelper.scala
+++ src/main/scala/com/metl/view/ReportHelper.scala
@@ -5,69 +5,87 @@ import java.util.Date
 
 import com.github.tototoshi.csv.CSVWriter
 import com.metl.data.ServerConfiguration
-import com.metl.model.{ConversationRoom, MeTLXConfiguration}
+import com.metl.model.{ConversationRoom, GlobalRoom, MeTLXConfiguration}
+import net.liftweb.mapper.DB
 
-case class RawRow(author:String, conversationJid:Int, index:Int, timestamp:Long, present:Boolean, activity:Long=0)
+case class RawRow(author: String, conversationJid: Int, location: String, index: Int, timestamp: Long, present: Boolean, activity: Long = 0)
+
+case class ProcessedRow(author: String, conversationJid: Int, location: String, index: Int, duration: Int, approx: Boolean, visits: Int, activity: Long)
 
 object ReportHelper {
 
-  /* Activity on page is number of h2ink, h2multiwordtext, h2image, h2quizresponse for that page in conv */
-  def getRoomActivity(conversationJid: Int, index: Int):Long = {
-    0
+  /* Activity on page is number of h2ink, h2multiwordtext, h2image for that page in conv */
+  def getRoomActivity(author: String, location: String): Long = {
+    // results: (List[String] headers, List[List[String]] data)
+    val results = DB.runQuery("select (" +
+      "(select count(*) from h2ink where author = ? and slide = ?) + " +
+      "(select count(*) from h2multiwordtext where author = ? and slide = ?) + " +
+      "(select count(*) from h2image where author = ? and slide = ?)" +
+      ") as total", List.fill(3)(List(author, location)).flatten)
+    results._2.head.head.toLong
   }
 
-  /* Traverse via incrementing counter.
-    Increment on enter, decrement on exit, >= 1 is "in", <= 0 is "out".
-    Presume some sensible "reset moment" if possible server crash or restart. */
-  def getSecondsOnPage(rows:List[RawRow]):(Int,Boolean) = {
+  /* Traverse via incrementing counter. Increment on enter, decrement on exit, >= 1 is "in", <= 0 is "out". */
+  def getSecondsOnPage(rows: List[RawRow]): (Int, Boolean) = {
     var counter = 0
-    for(r <- rows) {
-      if(r.present) counter += 1 else counter -= 1
+    for (r <- rows) {
+      if (r.present) counter += 1 else counter -= 1
     }
-    //    rows.reverse.foldLeft(counter)(_.timestamp + _.timestamp  )
     (counter, counter > 0)
   }
 
-  def studentActivity(courseId:String):String = {
+  def studentActivity(courseId: String): String = {
     println("Generating student activity...")
     val start = new Date().toInstant
 
     val server = ServerConfiguration.default
     val conversations = server.searchForConversationByCourse(courseId)
-    println("Conversations: " + conversations.length)
 
-    var rawRows:List[RawRow] = List()
+    /*
+        val globalRoom = MeTLXConfiguration.getRoom("global", server.name, GlobalRoom(server.name))
+        val conversationAttendances = globalRoom.getHistory.getAttendances
+        println( "Conversation attendances: " + conversationAttendances.length)
+    */
+
+    var rawRows: List[RawRow] = List()
     conversations.foreach(conversation => {
       val room = MeTLXConfiguration.getRoom(conversation.jid.toString, server.name, ConversationRoom(server.name, conversation.jid.toString))
-      println( "Room location: " + room.location)
 
       // Sort attendances just in case.
-      val attendances = room.getAttendances.sortBy(_.timestamp)
-      println( "Attendances: " + attendances.length)
+      val slideAttendances = room.getHistory.getAttendances.sortBy(_.timestamp)
 
-      conversation.slides.foreach(s => println("Slide" + s.index + " id: " + s.id))
+      println("Room " + room.location + ", " + slideAttendances.length + "attendances")
 
-      attendances.foreach(a => {
-        println("Attendance location: " + a.location)
-
+      slideAttendances.foreach(a => {
         // Translate a.location (jid) into index via conversation.slides
         val index = conversation.slides.find(_.id == a.location.toInt).map(_.index + 1).getOrElse(0)
-
-        val newRow = RawRow(a.author, conversation.jid, index, a.timestamp, a.present)
+        val newRow = RawRow(a.author, conversation.jid, a.location, index, a.timestamp, a.present)
         rawRows = newRow :: rawRows
       })
     })
 
-    // TODO: split rawRows by (1,2,3) then group results
+    // Split rawRows by (1,2,3) to give a Map of Lists that have the same (1,2,3)
+    val grouped = rawRows.reverse.groupBy(r => (r.author, r.conversationJid, r.location))
+
+    var processedRows: List[ProcessedRow] = List()
+    grouped.foreach(g => {
+      val head = g._2.head
+      val duration = getSecondsOnPage(g._2)
+      processedRows = ProcessedRow(head.author, head.conversationJid, head.location, head.index,
+        duration._1, duration._2, g._2.length,
+        getRoomActivity(head.author, head.location)) :: processedRows
+    })
 
     // Reassemble CSV rows from rawRows
-    var rows:List[List[String]] = List(List())
-    rawRows.reverse.foreach(r => {
+    var rows: List[List[String]] = List(List())
+    processedRows.reverse.foreach(r => {
       rows = List(r.author,
         r.conversationJid.toString,
         r.index.toString,
-        r.timestamp.toString,
-        getRoomActivity(r.conversationJid, r.index).toString) :: rows
+        r.duration.toString,
+        r.visits.toString,
+        r.activity.toString,
+        r.approx.toString) :: rows
     })
 
     val stringWriter = new StringWriter()
