1af667a34d74b988ed8d1f610ae1d4c09c854b74
Doug Hagan <doug@stackableregiments.com>
2017-03-09 11:01:22 +1100

    METL-569: Introduced case class and helper functions.

--- src/main/scala/com/metl/view/ReportHelper.scala
+++ src/main/scala/com/metl/view/ReportHelper.scala
@@ -1,82 +1,82 @@
 package com.metl.view
 
 import java.io.StringWriter
+import java.util.Date
 
 import com.github.tototoshi.csv.CSVWriter
 import com.metl.data.ServerConfiguration
 import com.metl.model.{ConversationRoom, MeTLXConfiguration}
 
+case class RawRow(author:String, conversationJid:Int, index:Int, timestamp:Long, present:Boolean, activity:Long=0)
+
 object ReportHelper {
+
+  /* Activity on page is number of h2ink, h2multiwordtext, h2image, h2quizresponse for that page in conv */
+  def getRoomActivity(conversationJid: Int, index: Int):Long = {
+    0
+  }
+
+  /* Traverse via incrementing counter.
+    Increment on enter, decrement on exit, >= 1 is "in", <= 0 is "out".
+    Presume some sensible "reset moment" if possible server crash or restart. */
+  def getSecondsOnPage(rows:List[RawRow]):(Int,Boolean) = {
+    var counter = 0
+    for(r <- rows) {
+      if(r.present) counter += 1 else counter -= 1
+    }
+    //    rows.reverse.foldLeft(counter)(_.timestamp + _.timestamp  )
+    (counter, counter > 0)
+  }
+
   def studentActivity(courseId:String):String = {
     println("Generating student activity...")
-
-    val stringWriter = new StringWriter()
-    val writer = CSVWriter.open(stringWriter)
+    val start = new Date().toInstant
 
     val server = ServerConfiguration.default
     val conversations = server.searchForConversationByCourse(courseId)
     println("Conversations: " + conversations.length)
 
-    var rawRows:List[List[String]] = List(List())
-    conversations.foreach(c => {
-      val r = MeTLXConfiguration.getRoom(c.jid.toString, server.name, ConversationRoom(server.name, c.jid.toString))
-      println( "Room: " + r.location)
+    var rawRows:List[RawRow] = List()
+    conversations.foreach(conversation => {
+      val room = MeTLXConfiguration.getRoom(conversation.jid.toString, server.name, ConversationRoom(server.name, conversation.jid.toString))
+      println( "Room location: " + room.location)
 
       // Sort attendances just in case.
-      val attendances = r.getAttendances.sortBy(_.timestamp)
+      val attendances = room.getAttendances.sortBy(_.timestamp)
       println( "Attendances: " + attendances.length)
 
-      c.slides.foreach(s => println("Slide" + s.index + " id: " + s.id))
+      conversation.slides.foreach(s => println("Slide" + s.index + " id: " + s.id))
 
       attendances.foreach(a => {
-        // Translate a.location (jid) into index via c.slides
         println("Attendance location: " + a.location)
 
-        val index = c.slides.find(_.id == a.location).map(_.index).getOrElse(0)
-        val newRow:List[String] = List(a.author, c.jid.toString, index.toString, a.timestamp.toString)
+        // Translate a.location (jid) into index via conversation.slides
+        val index = conversation.slides.find(_.id == a.location.toInt).map(_.index + 1).getOrElse(0)
+
+        val newRow = RawRow(a.author, conversation.jid, index, a.timestamp, a.present)
         rawRows = newRow :: rawRows
       })
     })
 
-    // Traverse via incrementing counter.
-    // Increment on enter, decrement on exit, >= 1 is "in", <= 0 is "out".
-    // Presume some sensible "reset moment" if possible server crash or restart.
+    // TODO: split rawRows by (1,2,3) then group results
+
+    // Reassemble CSV rows from rawRows
     var rows:List[List[String]] = List(List())
     rawRows.reverse.foreach(r => {
-      val duration = 0
+      rows = List(r.author,
+        r.conversationJid.toString,
+        r.index.toString,
+        r.timestamp.toString,
+        getRoomActivity(r.conversationJid, r.index).toString) :: rows
     })
 
-    // Activity on page is number of h2ink, h2multiwordtext, h2image, h2quizresponse for that page in conv
-
+    val stringWriter = new StringWriter()
+    val writer = CSVWriter.open(stringWriter)
     writer.writeRow(List("StudentID", "ConversationID", "PageLocation", "SecondsOnPage", "VisitsToPage", "ActivityOnPage", "Approximation"))
-    rows.foreach(r => writer.writeRow(r))
-
-    //      val providers = Globals.getGroupsProviders
-    //      providers.foreach(p => writer.writeRow(List(p.storeId, p.name, """here, "it" is""", "5,2,3", "2", "lots", "y")))
-
-
-    /*
-          writer.writeRow(List("jid", "title", "subject", "fr.key"))
-          //      val conversations = server.searchForConversation("d")
-          conversations.foreach(c =>
-            {
-              writer.writeRow(List(c.jid, c.title, c.subject, c.foreignRelationship.map(_.key).getOrElse("")))
-            })
-    */
-
-    /*
-          val provider = Globals.getGroupsProvider("d2l_test")
-          provider.foreach({
-            p =>
-              println("Found groups provider: " + p.name)
-    //          val orgUnit = p.getOrgUnit("CO_SWK-640-MWOL6-2017SP1")
-              val orgUnit = p.getOrgUnit(courseId)
-              orgUnit.foreach(ou => println("Found org unit: " + ou.name))
-          })
-    */
+    rows.reverse.foreach(r => writer.writeRow(r))
     writer.close()
 
-    println("Generated student activity.")
+    println("Generated student activity in %ds".format(new Date().toInstant.getEpochSecond - start.getEpochSecond))
     stringWriter.toString
   }
 }
