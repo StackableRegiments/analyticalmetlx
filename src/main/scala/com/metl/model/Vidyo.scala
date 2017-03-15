9f0884862c67555966d15ace380df342f694145a
David Hagan <dave@stackableregiments.com>
2017-03-15 16:51:58 +1100

    METL-579: added a token generator for vidyo, and removed the unnecessary parts of sbt.bat

---
+++ src/main/scala/com/metl/model/Vidyo.scala
@@ -0,0 +1,27 @@
+package com.metl.model
+
+import net.liftweb._
+import http._
+import util._
+import common._
+import Helpers._
+import json._
+import JsonDSL._
+import java.util.Date
+import org.apache.commons.codec.binary.Base64
+import org.apache.commons.codec.digest.HmacUtils
+
+object VidyoTokenProvider extends VidyoTokenProvider("","") 
+class VidyoTokenProvider(applicationId:String,developerKey:String){
+  protected val provisionToken = "provision"
+  protected val epochSeconds = 62167219200L
+  protected val delimiter = "\0"
+  def generateToken(username:String,secondsUntilExpiry:Long):String = {
+    val expiresInSeconds:Long = epochSeconds + (new Date().getTime / 1000) + secondsUntilExpiry
+    val jid = "%s@%s".format(username,applicationId)
+    val payload = List(provisionToken,jid,expiresInSeconds.toString).mkString(delimiter)
+    new String(Base64.encodeBase64(
+      (List(payload,HmacUtils.hmacSha384Hex(developerKey,payload)).mkString(delimiter)).getBytes()
+    ))
+  }
+}
