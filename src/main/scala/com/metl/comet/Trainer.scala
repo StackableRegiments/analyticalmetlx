dc4e426dbfa434470fa5a594b852cf113ac582cc
Chris Hagan <chris@stackableregiments.com>
2017-06-26 16:47:24 -0400

    METL-644: Added actioned state to controls

--- src/main/scala/com/metl/comet/Trainer.scala
+++ src/main/scala/com/metl/comet/Trainer.scala
@@ -8,6 +8,7 @@ import net.liftweb._
 import net.liftweb.json._
 import common._
 import http._
+import net.liftweb.http.js.JsCmds.SetHtml
 import util._
 import Helpers._
 import HttpHelpers._
@@ -26,8 +27,16 @@ import net.liftweb.http.js.jquery.JqJE._
 
 trait TrainingBlock
 case class TrainingInstruction(content:NodeSeq) extends TrainingBlock
-case class TrainingControl(label:String,behaviour:()=>JsCmd) extends TrainingBlock
-case class TrainingPage(title:NodeSeq,blurb:NodeSeq,blocks:Seq[TrainingBlock],onLoad:Box[JsCmd])
+case class TrainingControl(label:String,behaviour:()=>JsCmd) extends TrainingBlock {
+  var isActioned = false
+  var progressMarker = 0
+  var maxProgress = 0
+  def actioned = isActioned = true
+  def progress = progressMarker = progressMarker + 1
+}
+case class TrainingPage(title:NodeSeq,blurb:NodeSeq,blocks:Seq[TrainingBlock],onLoad:Box[JsCmd]) {
+  def receiveStanza(s:MeTLStanza):Unit = ()
+}
 
 case object SimulatorTick
 
@@ -272,20 +281,39 @@ class TrainerActor extends StronglyTypedJsonActor with Logger {
     currentSlide = Full(slide)
 
     val location = newConversation.jid
-    // Don't need to actually store the messageBus
-    serverConfig.getMessageBus(new MessageBusDefinition(location.toString, "unicastBackToOwner", (m:MeTLStanza) => {
-      println("Received stanza at: " + m.timestamp)
-    }))
-
+    serverConfig.getMessageBus(new MessageBusDefinition(location.toString, "unicastBackToOwner", currentPage.receiveStanza _))
     Schedule.schedule(this,SimulatorTick,500)
   }
 
+  def blockMarkup:NodeSeq = currentPage.blocks.map(b => {
+    val id = nextFuncName
+    NodeSeq.fromSeq(List(
+      b match {
+        case c:TrainingControl => <div class="actioned">{
+          NodeSeq.fromSeq(List(
+            c.isActioned match {
+              case true => <input type="checkbox" id={id} disabled="true" checked="checked" />
+              case _ => <input type="checkbox" id={id} disabled="true" />
+            },
+            <label for={id}><span class="icon-txt"></span></label>))
+        }</div>
+        case _ => <span />
+      },
+      <div class="control">{
+        b match {
+          case c:TrainingControl =>
+            ajaxButton(c.label,() => {
+              c.actioned
+              c.behaviour()
+              SetHtml("exerciseControls",blockMarkup)
+            },"class" -> "active")
+          case c:TrainingInstruction => c.content
+        }
+      }</div>
+    ))}).reduce(_ ++ _)
   override def render = "#exerciseTitle *" #> currentPage.title &
   "#exerciseBlurb *" #> currentPage.blurb &
-  "#exerciseControls .control *" #> currentPage.blocks.map {
-    case c:TrainingControl => ajaxButton(c.label,c.behaviour,"class" -> "active")
-    case c:TrainingInstruction => c.content
-  } &
+  "#exerciseControls *" #> blockMarkup &
   "#scriptContainer *" #> (for {
     c <- currentConversation
     s <- currentSlide
