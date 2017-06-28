77d6abfca37663fbc64599efd5ccaf624c04743a
Doug Hagan <doug@stackableregiments.com>
2017-06-28 20:59:37 +1000

    METL-644: METL-644: Starting to bring in simulated Typing.

--- src/main/scala/com/metl/comet/Trainer.scala
+++ src/main/scala/com/metl/comet/Trainer.scala
@@ -82,6 +82,7 @@ import ScanDirections._
 trait SimulatedActivity
 case class Watching(ticks:Int) extends SimulatedActivity
 case class Scribbling(what:List[Char]) extends SimulatedActivity
+case class Typing(what:List[Char]) extends SimulatedActivity
 
 case class Highlight(selector:String)
 case class Flash(selector:String)
@@ -269,7 +270,7 @@ class TrainerActor extends StronglyTypedJsonActor with Logger {
     }
   }
 
-  override def lowPriority  = {
+  override def lowPriority = {
     case Highlight(selector) => partialUpdate(Call("Trainer.highlight",JString(selector)).cmd)
     case Flash(selector) => partialUpdate(Call("Trainer.flash",JString(selector)).cmd)
     case ShowClick(selector) => partialUpdate(Call("Trainer.showClick",JString(selector)).cmd)
@@ -296,7 +297,9 @@ class TrainerActor extends StronglyTypedJsonActor with Logger {
             }
             val claim = findFreeSpace(width, direction,users.map(_.claim))
             furtherClaimsAllowed = false
+            // Kick off Typing instead of Scribbling. How to choose which?
             u.copy(activity=Scribbling(name.toLowerCase.toList), focus=Point(claim.left,claim.top,0), claim=claim, attention=direction)
+//            u.copy(activity=Typing(name.toLowerCase.toList), focus=Point(claim.left,claim.top,0), claim=claim, attention=direction)
           }
           case _ => u.copy(activity = Watching(ticks + 1))
         }
@@ -305,17 +308,35 @@ class TrainerActor extends StronglyTypedJsonActor with Logger {
         case u@SimulatedUser(name,claim,focus,_,intention,Scribbling(h :: t),history) => Alphabet.geometry(h,focus) match {
           case g => {
             currentSlide.map(slide => {
+              val room = MeTLXConfiguration.getRoom(slide, server)
+              g.strokes.map(geometry => room ! LocalToServerMeTLStanza(MeTLInk(serverConfig, name, new java.util.Date().getTime, sum, sum,
+                geometry, intention match {
+                  case "benign" => Color(255, 255, 0, 0)
+                  case "malicious" => Color(255, 0, 0, 255)
+                  case _ => Color(255, 0, 255, 0)
+                }, 2.0, false, "presentationSpace", Privacy.PUBLIC, slide.toString, nextFuncName)))
+            })
+            u.pan(g.width).copy(activity = Scribbling(t), history = Scribbling(List(h)) :: u.history)
+          }
+        }
+        case u@SimulatedUser(name,claim,focus,_,intention,Typing(Nil),history) => u.copy(activity = Watching(0))
+        case u@SimulatedUser(name,claim,focus,_,intention,Typing(h :: t),history) if h == ' ' => u.pan(Alphabet.space.width).copy(activity = Typing(t),history = Typing(List(h)) :: u.history)
+/*
+        case u@SimulatedUser(name,claim,focus,_,intention,Typing(h :: t),history) => Alphabet.geometry(h,focus) match {
+          case g => {
+            currentSlide.map(slide => {
               val room = MeTLXConfiguration.getRoom(slide,server)
-              g.strokes.map(geometry => room ! LocalToServerMeTLStanza(MeTLInk(serverConfig,name,new java.util.Date().getTime,sum,sum,
-                geometry,intention match {
+              g.strokes.map(geometry => room ! LocalToServerMeTLStanza(MeTLSingleChar(serverConfig,name,new java.util.Date().getTime,"b",x,y,width,height,fontFamily,fontSize,
+                intention match {
                   case "benign" => Color(255,255,0,0)
                   case "malicious" => Color(255,0,0,255)
                   case _ => Color(255,0,255,0)
-                },2.0,false,"presentationSpace",Privacy.PUBLIC,slide.toString,nextFuncName)))
+                },2.0,nextFuncName,"presentationSpace",Privacy.PUBLIC,slide.toString)))
             })
-            u.pan(g.width).copy(activity = Scribbling(t),history = Scribbling(List(h)) :: u.history)
+            u.pan(g.width).copy(activity = Scribbling(t),history = Typing(List(h)) :: u.history)
           }
         }
+*/
       }
       partialUpdate(Call("Trainer.simulatedUsers",Extraction.decompose(users)).cmd)
       Schedule.schedule(this,SimulatorTick,1000)
