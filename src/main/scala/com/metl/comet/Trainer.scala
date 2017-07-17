8d87f9989563184e9a7f2a50052536bffb999b31
Doug Hagan <doug@stackableregiments.com>
2017-07-17 16:24:13 +1000

    METL-240: Separated data (DSL defining TrainingManual) from supporting code. Moved CSS into training.css. (Renamed to match html.) Restyled checkboxes.

--- src/main/scala/com/metl/comet/Trainer.scala
+++ src/main/scala/com/metl/comet/Trainer.scala
@@ -13,7 +13,6 @@ import Helpers._
 import scala.xml._
 import com.metl.model._
 import SHtml._
-import org.fluttercode.datafactory.impl._
 import js._
 import JsCmds._
 import JE._
@@ -81,7 +80,6 @@ object Intentions {
 
 import ScanDirections._
 
-
 trait SimulatedActivity
 case class Watching(ticks:Int) extends SimulatedActivity
 case class Scribbling(what:List[Char]) extends SimulatedActivity
@@ -94,186 +92,6 @@ case class ShowClick(selector:String)
 case class ClaimedArea(left:Double,top:Double,right:Double,bottom:Double,width:Double)
 case class SimulatedUser(name:String,claim:ClaimedArea,focus:Point,attention:ScanDirection,intention:Intention,activity:SimulatedActivity,history:List[SimulatedActivity]){
   def pan(x:Int,y:Int = 0) = copy(focus = Point(focus.x + x,focus.y + y,0))
-}
-
-case class TrainingManual(actor:TrainerActor) {
-  import Dimensions._
-  val namer = new DataFactory
-  def el(label:String,content:String) = Elem.apply(null,label,scala.xml.Null,scala.xml.TopScope,Text(content))
-  def p(content:String) = TrainingInstruction(el("p",content))
-  def makeStudent(intent:Intention) = TrainingControl(
-    "Bring in some students".format(intent),c => {
-      c.progress
-      actor.users = SimulatedUser("%s %s".format(namer.getFirstName, namer.getLastName),ClaimedArea(0,0,0,lineHeight,0),Point(0,0,0),Below,Intentions.Benign,Watching(0),List.empty[SimulatedActivity]) :: actor.users
-    }).reps(3)
-  val networkMonitor:TrainingControl = TrainingControl(
-    "How can I tell if there's a problem?",
-    _ => {
-      actor ! Flash(".meters")
-    }).supplementary(List(
-      "These three meters indicate three kinds of health.",
-      "The topmost meter watches for a healthy network connection, checking every time you do any action and also checking in the background periodically.  If the network ever fails health check completely, a red boundary will appear around these meters and remain red for the next five minutes.  This is to make you aware of a poor environment.",
-      "The center meter is about whether all of your students are with you.  This measures the number of students who are allowed to attend this session against the number of students who have joined the conversation.  This is only effective when your conversation is restricted to a particular enrolment context.  In this particular case, you are in a private conversation; only you are allowed to join, and only you are here.  One hundred percent!",
-      "The third meter measures overall activity in the room.  The green curve above the line indicates public activity, and the red curve below the line measures private activity.  This is the only interaction you are permitted to have with private content."
-    ))
-  val inkTracker:TrainingControl = TrainingControl(
-    "Draw a few strokes",
-    _ => {
-      actor ! new StanzaTrigger(s => {
-        s match {
-          case i: MeTLInk if actor isHuman i =>
-            inkTracker.progress
-            actor ! RefreshControls
-          case _ =>
-        }
-        s
-      })
-      actor ! ShowClick("#drawMode")
-    }
-  ).reps(3)
-  val textTracker:TrainingControl = TrainingControl(
-    "Write some words",
-    _ => {
-      actor ! new StanzaTrigger(s => {
-        s match {
-          case i: MeTLMultiWordText if actor isHuman i =>
-            textTracker.progress
-            actor ! RefreshControls
-          case _ =>
-        }
-        s
-      })
-      actor ! ShowClick("#insertText")
-    }
-  ).reps(1)
-  val imageTracker:TrainingControl = TrainingControl(
-    "Insert an image",
-    _ => {
-      actor ! new StanzaTrigger(s => {
-        s match {
-          case i: MeTLImage if actor isHuman i =>
-            imageTracker.progress
-            actor ! RefreshControls
-          case _ =>
-        }
-        s
-      })
-      actor ! ShowClick("#insertMode")
-    }
-  ).reps(1)
-  val pages:List[TrainingPage] = List(
-    TrainingPage(Text("Exercise 1"),
-      Text("The teaching space"),
-      List(
-        TrainingNavigator(
-          "Dev Mode: jump to page 3",
-          _ => actor ! pages(2)
-        ),
-        p("This space is a whiteboard on which you and your class can write."),
-        p("Like a slide deck or a PowerPoint presentation, it supports multiple pages."),
-        TrainingControl(
-          "Show me the pages",
-          _ => actor ! Flash("#thumbsColumn")
-        ),
-        p("You may choose whether your class can move freely between these."),
-        TrainingControl("How do I move between pages?",
-          _ => actor ! Flash("#slideControls")
-        ),
-        p("Only the author of the conversation can add pages."),
-        TrainingControl(
-          "Show me how",
-          _ => {
-            Schedule.schedule(actor,Flash("#addSlideButton"),1000)
-            actor ! Highlight("#slideControls")
-          }
-        ),
-        p("Classroom spaces, the device you're using and network speed can affect whether the whiteboard works well for you."),
-        networkMonitor,
-        p("In the next exercise, we'll do some work on the pages"),
-        TrainingNavigator(
-          "Take me there",
-          _ => actor ! pages(1)
-        )),
-      Full(
-        Call("Trainer.clearTools").cmd
-      ),
-      List(new StanzaTrigger((stanza:MeTLStanza) => { actor.logStanza(stanza,"Exercise 1")}))
-    ),
-    TrainingPage(Text("Exercise 2"),
-      Text("Sharing an open space"),
-      List(
-        p("In this trainer you can bring virtual students into your classroom.  These students will stay with you during your training session."),
-        makeStudent(Intentions.Benign),
-        p("The students you create can work anywhere, even outside of where you are currently looking."),
-        p("To observe all their work, set your camera to include all content no matter where it appears."),
-        TrainingControl(
-          "Show me how to watch everything",
-          _ => {
-            Schedule.schedule(actor,ShowClick("#zoomToFull"),1000)
-            actor ! ShowClick("#zoomMode")
-          }
-        ),
-        p("If you just want to concentrate on your own work, set your camera not to move automatically."),
-        TrainingControl(
-          "Show me how to stop the camera moving",
-          _ => {
-            Schedule.schedule(actor,ShowClick("#zoomToCurrent"),1000)
-            actor ! ShowClick("#zoomMode")
-          }
-        ),
-        p("Now let's make some of your own content."),
-        TrainingNavigator(
-          "Show me the rest of the tools",
-          _ => actor ! pages(2)
-        ),
-        TrainingNavigator(
-          "Show me exercise 1 again",
-          _ => actor ! pages(0)
-        )
-      ),
-      Full(
-        Call("Trainer.clearTools").cmd &
-          Call("Trainer.highlight","#toolsColumn").cmd &
-          Call("Trainer.hide",".permission-states").cmd &
-          Call("Trainer.hide","#floatingToggleContainer").cmd &
-          Call("Trainer.hide",".meters").cmd
-      ),
-      List(new StanzaTrigger((stanza:MeTLStanza) => { actor.logStanza(stanza,"Exercise 2")}))
-    ),
-    TrainingPage(Text("Exercise 3"),
-      Text("Your creative space"),
-      List(
-        p("You have control over whether your content appears to other users."),
-        TrainingControl(
-          "Which controls do that?",
-          _ => {
-            actor ! Highlight("#toolsColumn")
-            actor ! Flash(".permission-states")
-          }
-        ),
-        p("You can add several kinds of content to the space.  Your selection of Public versus Private at the time you add new content will determine whether that content is visible to others.  It is always visible to you, and you can change your mind later."),
-        p("Try drawing some lines.  You can use your finger, or a stylus, or a mouse."),
-        inkTracker,
-        p("Try writing some text."),
-        textTracker,
-        p("Try inserting an image.  You can use any image stored on your device."),
-        imageTracker,
-        p("Once you have added content, you may need to move it, resize it, hide or show it."),
-        TrainingControl(
-          "Show me how to modify existing content",
-          _ => {
-            actor ! ShowClick("#selectMode")
-          }
-        ),
-        TrainingNavigator(
-          "Show me exercise 2 again",
-          _ => actor ! pages(1)
-        )
-      ),
-      Full(Call("Trainer.showTools").cmd),
-      List(new StanzaTrigger((stanza:MeTLStanza) => { actor.logStanza(stanza,"Exercise 3")}))
-    )
-  )
 }
 
 class TrainerActor extends StronglyTypedJsonActor with Logger {
@@ -493,8 +311,8 @@ class TrainerActor extends StronglyTypedJsonActor with Logger {
     val id = nextFuncName
     NodeSeq.fromSeq(List(
       checked match {
-        case true => <input type="checkbox" id={id} disabled="true" checked="checked" />
-        case _ => <input type="checkbox" id={id} disabled="true" />
+        case true => <input type="checkbox" class="action" id={id} disabled="true" checked="checked" />
+        case _ => <input type="checkbox" class="action" id={id} disabled="true" />
       },
       <label for={id}><span class="icon-txt"></span></label>))
   }
@@ -512,7 +330,7 @@ class TrainerActor extends StronglyTypedJsonActor with Logger {
         </div>
       }
       else {
-          <span/>
+        <span/>
       }
 
       val label = ajaxButton(tc.label, () => {
---
+++ src/main/scala/com/metl/comet/TrainingManual.scala
@@ -0,0 +1,201 @@
+package com.metl.comet
+
+import com.metl.comet.ScanDirections.Below
+import com.metl.data._
+import net.liftweb.common.Full
+import net.liftweb.http.js.JE.Call
+import net.liftweb.util.Schedule
+import org.fluttercode.datafactory.impl.DataFactory
+
+import scala.xml.{Elem, Text}
+
+case class TrainingManual(actor:TrainerActor) {
+  import Dimensions._
+  val namer = new DataFactory
+  def el(label:String,content:String) = Elem.apply(null,label,scala.xml.Null,scala.xml.TopScope,Text(content))
+  def p(content:String) = TrainingInstruction(el("p",content))
+  def makeStudent(intent:Intention) = TrainingControl(
+    "Bring in some students".format(intent),c => {
+      c.progress
+      actor.users = SimulatedUser("%s %s".format(namer.getFirstName, namer.getLastName),ClaimedArea(0,0,0,lineHeight,0),Point(0,0,0),Below,Intentions.Benign,Watching(0),List.empty[SimulatedActivity]) :: actor.users
+    }).reps(3)
+  val inkTracker:TrainingControl = TrainingControl(
+    "Draw a few strokes",
+    _ => {
+      actor ! new StanzaTrigger(s => {
+        s match {
+          case i: MeTLInk if actor isHuman i =>
+            inkTracker.progress
+            actor ! RefreshControls
+          case _ =>
+        }
+        s
+      })
+      actor ! ShowClick("#drawMode")
+    }
+  ).reps(3)
+  val textTracker:TrainingControl = TrainingControl(
+    "Add a couple of text boxes",
+    _ => {
+      actor ! new StanzaTrigger(s => {
+        s match {
+          case i: MeTLMultiWordText if actor isHuman i =>
+            textTracker.progress
+            actor ! RefreshControls
+          case _ =>
+        }
+        s
+      })
+      actor ! ShowClick("#insertText")
+    }
+  ).reps(2)
+  val imageTracker:TrainingControl = TrainingControl(
+    "Insert an image",
+    _ => {
+      actor ! new StanzaTrigger(s => {
+        s match {
+          case i: MeTLImage if actor isHuman i =>
+            imageTracker.progress
+            actor ! RefreshControls
+          case _ =>
+        }
+        s
+      })
+      actor ! ShowClick("#insertMode")
+    }
+  ).reps(1)
+
+  class TrainingReturnTo(pageNumber:Int)
+    extends TrainingNavigator("Take me back to exercise " + pageNumber, _ => actor ! pages(pageNumber - 1))
+
+  val pages:List[TrainingPage] = List(
+    TrainingPage(Text("Exercise 1"),
+      Text("The teaching space"),
+      List(
+        p("This space is a whiteboard on which you and your class can write."),
+        p("Like a slide deck or a PowerPoint presentation, it supports multiple pages."),
+        TrainingControl(
+          "Show me the pages",
+          _ => actor ! Flash("#thumbsColumn")
+        ),
+        p("You may choose whether your class can move freely between these."),
+        TrainingControl("How do I move between pages?",
+          _ => actor ! Flash("#slideControls")
+        ),
+        p("Only the author of the conversation can add pages."),
+        TrainingControl(
+          "Show me how",
+          _ => {
+            Schedule.schedule(actor,Flash("#addSlideButton"),1000)
+            actor ! Highlight("#slideControls")
+          }
+        ),
+        p("Classroom spaces, the device you're using and network speed can affect whether the whiteboard works well for you."),
+        TrainingControl(
+          "How can I tell if there's a problem?",
+          _ => {
+            actor ! Flash(".meters")
+          }).supplementary(List(
+          "These three meters indicate three kinds of health.",
+          "The topmost meter watches for a healthy network connection, checking every time you do any action and also checking in the background periodically.  If the network ever fails health check completely, a red boundary will appear around these meters and remain red for the next five minutes.  This is to make you aware of a poor environment.",
+          "The center meter is about whether all of your students are with you.  This measures the number of students who are allowed to attend this session against the number of students who have joined the conversation.  This is only effective when your conversation is restricted to a particular enrolment context.  In this particular case, you are in a private conversation; only you are allowed to join, and only you are here.  One hundred percent!",
+          "The third meter measures overall activity in the room.  The green curve above the line indicates public activity, and the red curve below the line measures private activity.  This is the only interaction you are permitted to have with private content."
+        )),
+        p("In the next exercise, we'll do some work on the pages."),
+        TrainingNavigator(
+          "Show me how to create content",
+          _ => actor ! pages(1)
+        )),
+      Full(
+        Call("Trainer.clearTools").cmd
+      ),
+      List(new StanzaTrigger((stanza:MeTLStanza) => { actor.logStanza(stanza,"Exercise 1")}))
+    ),
+    TrainingPage(Text("Exercise 2"),
+      Text("Sharing an open space"),
+      List(
+        p("In this trainer you can bring virtual students into your classroom.  These students will stay with you during your training session."),
+        makeStudent(Intentions.Benign),
+        p("The students you create can work anywhere, even outside of where you are currently looking."),
+        p("To observe all their work, set your camera to include all content no matter where it appears."),
+        TrainingControl(
+          "Show me how to watch everything",
+          _ => {
+            Schedule.schedule(actor,ShowClick("#zoomToFull"),1000)
+            actor ! ShowClick("#zoomMode")
+          }
+        ),
+        p("If you just want to concentrate on your own work, set your camera not to move automatically."),
+        TrainingControl(
+          "Show me how to stop the camera moving",
+          _ => {
+            Schedule.schedule(actor,ShowClick("#zoomToCurrent"),1000)
+            actor ! ShowClick("#zoomMode")
+          }
+        ),
+        p("Now let's make some of your own content."),
+        TrainingNavigator(
+          "Show me the rest of the tools",
+          _ => actor ! pages(2)
+        ),
+        new TrainingReturnTo(1)
+      ),
+      Full(
+        Call("Trainer.clearTools").cmd &
+          Call("Trainer.highlight","#toolsColumn").cmd &
+          Call("Trainer.hide",".permission-states").cmd &
+          Call("Trainer.hide","#floatingToggleContainer").cmd &
+          Call("Trainer.hide",".meters").cmd
+      ),
+      List(new StanzaTrigger((stanza:MeTLStanza) => { actor.logStanza(stanza,"Exercise 2")}))
+    ),
+    TrainingPage(Text("Exercise 3"),
+      Text("Your creative space"),
+      List(
+        p("You have control over whether your content appears to other users (Public) or just yourself (Private)."),
+        TrainingControl(
+          "Which controls do that?",
+          _ => {
+            actor ! Highlight("#toolsColumn")
+            actor ! Flash(".permission-states")
+          }
+        ).supplementary(List(
+          "Your selection of Public versus Private at the time you add new content will determine whether that content is visible to others.",
+          "It is always visible to you, and you can change your mind later."
+        )),
+        p("You can add several kinds of content to the space."),
+        p("Try drawing some lines.  You can use your finger, or a stylus, or a mouse."),
+        inkTracker,
+        p("Try writing some text."),
+        textTracker,
+        p("Try inserting an image.  You can use any image stored on your device."),
+        imageTracker,
+        p("Once you have added content, you may need to move it, resize it, hide or show it."),
+        TrainingControl(
+          "Show me how to modify existing content",
+          _ => {
+            actor ! ShowClick("#selectMode")
+          }
+        ),
+        new TrainingReturnTo(2)
+      ),
+      Full(Call("Trainer.showTools").cmd),
+      List(new StanzaTrigger((stanza:MeTLStanza) => { actor.logStanza(stanza,"Exercise 3")}))
+    ),
+    TrainingPage(Text("Exercise 4"),
+      Text("Modifying your creations"),
+      List(
+        p("Once you have added content, you may need to move it, resize it, hide or show it."),
+        TrainingControl(
+          "Show me how to modify existing content",
+          _ => {
+            actor ! ShowClick("#selectMode")
+          }
+        ),
+        new TrainingReturnTo(3)
+      ),
+      Full(Call("Trainer.showTools").cmd),
+      List(new StanzaTrigger((stanza:MeTLStanza) => { actor.logStanza(stanza,"Exercise 3")}))
+    )
+  )
+}
