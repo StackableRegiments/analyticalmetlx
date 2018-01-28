ee4d8fac49243fbcf60e7b551f5745592dc362ca
Doug Hagan <doug@stackableregiments.com>
2018-01-28 21:07:48 +1100

    Added verbose output while minifying.

--- src/main/webapp/static/js/minify.sh
+++ src/main/webapp/static/js/minify.sh
@@ -2,9 +2,9 @@ echo "execute this from the src/main/webapp/static/js directory"
 for file in $(ls)
 do
     case $file in
-        *~) ;;
-        *.min.js) ;;
+        *~) echo not processing backup $file ;;
+        *.min.js) echo not processing minified $file ;;
 ##        *.js) java -jar /c/lang/closureCompiler/closure-compiler-v20160713.jar $file --js_output_file=min/$file ;;
-        *.js) java -jar ../../../../../tools/closure-compiler-v20160713.jar $file --js_output_file=min/$file ;;
+        *.js) processing $file ; java -jar ../../../../../tools/closure-compiler-v20160713.jar $file --js_output_file=min/$file ;;
     esac
 done
