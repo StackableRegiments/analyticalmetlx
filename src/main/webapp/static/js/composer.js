3ae0ed8acd6a33146f2ee5065c8e9538d96ebd26
Chris Hagan <chris@stackableregiments.com>
2018-05-07 21:37:10 +1000

    got viewport behaving on resize

--- src/main/webapp/static/js/composer.js
+++ src/main/webapp/static/js/composer.js
@@ -1,11 +1,10 @@
-var composer = (function(){
+var composer = function(){
     var host = $(".scene");
     var w = host.width();
     var h = host.height();
 
     var renderer = new THREE.WebGLRenderer();
     renderer.autoClear = false;
-    renderer.depthTest = false;
 
     renderer.setSize(w,h);
     var dom = renderer.domElement;
@@ -13,20 +12,49 @@ var composer = (function(){
 
     var renderTicks = {};
     var physicsTicks = {};
+    var shoots = [];
 
-    var render = function(){
+    var renderShoot = function(shoot){
+        renderer.render(shoot.scene,shoot.camera);
+        renderer.clearDepth();
+    }
+    var resizeShoots = function(){
         _.each(shoots,function(shoot){
-            renderer.render(shoot.scene,shoot.camera);
+            shoot.resize(w,h);
         });
+    };
+    var render = function(){
+        _.each(renderTicks,function(f){
+            f();
+        });
+        renderer.clear();
+        _.each(shoots,renderShoot);
         requestAnimationFrame(render);
     };
     return {
-        renderTicks:{},
-        physicsTicks:{},
-        shoots:[],
+        renderTicks:renderTicks,
+        physicsTicks:physicsTicks,
+        shoots:shoots,
         action:function(){
-	    console.log("Composer beginning render");
-	    render();
+            window.onresize = function(){
+                w = host.width();
+                h = host.height();
+		console.log("Resize",w,h);
+		var c = host.find("canvas");
+                c.attr("width",w);
+                c.attr("height",h);
+		c.css({
+		    width:""+w+"px",
+		    height:""+h+"px"
+		});
+		var ctx = c[0].getContext("webgl");
+		ctx.width = w;
+		ctx.height = h;
+		ctx.viewport(0,0,w,h);
+                resizeShoots(w,h);
+            };
+            resizeShoots(w,h);
+            render();
         }
     }
-})();
+};
--- src/main/webapp/static/js/majorModes.js
+++ src/main/webapp/static/js/majorModes.js
@@ -3,10 +3,14 @@ var majorModes = function(c){
 
     var scene = new THREE.Scene();
     var host = $(".scene");
-    var w = host.width() * 0.5;
-    var h = host.height() * 0.2;
-    var camera = new THREE.PerspectiveCamera( 75, w / h , 0.1, 1000 );
-
+    var camera = new THREE.PerspectiveCamera( 75, 4/3 , 0.1, 1000 );
+    var resize = function(w,h){
+        camera.setViewOffset(
+            w,h,
+	    w/-2,
+            h * -3,
+            w*4,h*4);
+    };
     var billboards = [];
 
     var circle = Math.PI*2;
@@ -23,10 +27,11 @@ var majorModes = function(c){
     var controlHost = new THREE.Object3D();
 
     var dist = circle/_.keys(controls).length;
+    var radius = host.height()/5;
     _.each(_.keys(controls),function(k,i){
         var v = controls[k];
         var controlM = new THREE.Mesh(new THREE.PlaneGeometry());
-        var visibleM = new THREE.Mesh(new THREE.PlaneGeometry(radius/1.5,radius/1.5));
+        var visibleM = new THREE.Mesh(new THREE.PlaneGeometry(radius / 2,radius / 2));
         controlPairs.push([visibleM,controlM]);
         controlM.position.set(Math.cos(i * dist) * radius,0,Math.sin(i * dist) * radius);
         controlHost.add(controlM);
@@ -70,6 +75,7 @@ var majorModes = function(c){
     };
     c.shoots.push({
         camera:camera,
-        scene: scene
+        scene: scene,
+        resize:resize
     });
 };
